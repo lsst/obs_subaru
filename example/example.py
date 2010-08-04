d99aac5a6fdd88d7dce29a5fdc50b94f92f6f1bd
Hisanori Furusawa <furusawa@subaru.naoj.org>
2010-08-04 11:02:18 +0900

    Modified: x,y_dist and x,y_undist were revereted in example.py

--- example/example.py
+++ example/example.py
@@ -4,21 +4,20 @@ import hsc.meas.match.distest as distest
 
 elevation = 30.0 
 
-x_dist = 10000.0 # pix from the fov center
-y_dist = 10000.0 # pix from the fov center
+x_undist = 10000.0 # pix from the fov center
+y_undist = 10000.0 # pix from the fov center
 
-x_undist, y_undist = distest.getDistortedPosition(x_dist, y_dist, elevation)
+x_dist, y_dist = distest.getDistortedPosition(x_undist, y_undist, elevation)
 
-print "distored position:   ( %lf , %lf )" % (x_dist, y_dist)
-print "==>"
 print "undistored position: ( %lf , %lf )" % (x_undist, y_undist)
+print "==>"
+print "distored position:   ( %lf , %lf )" % (x_dist, y_dist)
 
 print " ---- "
 
-x_dist, y_dist = distest.getUndistortedPosition(x_undist, y_undist, elevation)
+x_undist, y_undist = distest.getUndistortedPosition(x_dist, y_dist, elevation)
 
-print "undistored position: ( %lf , %lf )" % (x_undist, y_undist)
-print "==>"
 print "distored position:   ( %lf , %lf )" % (x_dist, y_dist)
+print "==>"
+print "undistored position: ( %lf , %lf )" % (x_undist, y_undist)
 
-                                        
