cb391cfa24206d637fab5fe39b035edf97f1c26a
price <price@git.lsstcorp.org>
2011-01-13 20:05:30 +0000

    Make flats appropriate for all times.  Eventually we'll turn this off, but it makes things easier for now. Write registry in the root directory. Allow filters other than SDSS.

--- bin/genCalibRegistry.py
+++ bin/genCalibRegistry.py
@@ -3,20 +3,19 @@
 import glob
 import os
 import re
-import sqlite3
+import sqlite
 import sys
 import math
 
 import lsst.daf.base   as dafBase
 import lsst.afw.image  as afwImage
 
-
-
 root = sys.argv[1]
+registry = os.path.join(root, "calibRegistry.sqlite3")
 
-if os.path.exists("calibRegistry.sqlite3"):
-    os.unlink("calibRegistry.sqlite3")
-conn = sqlite3.connect("calibRegistry.sqlite3")
+if os.path.exists(registry):
+    os.unlink(registry)
+conn = sqlite.connect(registry)
 
 
 # Flats
@@ -33,32 +32,35 @@ derivedRunId = 0
 runId = 0
 version = 0
 
-for fits in glob.glob(os.path.join(root, "FLAT", "20*-*-*", "W-S-*", "*", "FLAT-*.fits*")):
+for fits in glob.glob(os.path.join(root, "FLAT", "20*-*-*", "?-?-*", "*", "FLAT-*.fits*")):
     print fits
-    m = re.search(r'\w+/(\d{4}-\d{2}-\d{2})/(W-S-.{1,3})/(\d+)/\w+-(\d{7})(\d).fits', fits)
+    m = re.search(r'\w+/(\d{4}-\d{2}-\d{2})/(.-.-.{1,3})/(\d+)/\w+-(\d{7})(\d).fits', fits)
     if not m:
         print >>sys.stderr, "Warning: Unrecognized file:", fits
         continue
 
     dateObs, filter, mystery, flatnum, ccd = m.groups()
 
-    md = afwImage.readMetadata(fits)
-    expTime = md.get("EXPTIME")
-    mjdObs = md.get("MJD")
-
-    # Convert to local time to get meaningful validStart, validEnd
-    localObs = mjdObs - 10.0/24.0
-    validStart = math.floor(localObs - 0.5) + 0.5 + 10.0/24.0 # Local midday, in UTC
-    validEnd = validStart + 1.0
+    if False:
+        # Convert to local time to get meaningful validStart, validEnd
+        md = afwImage.readMetadata(fits)
+        mjdObs = md.get("MJD")
+        taiObs = dafBase.DateTime(mjdObs, dafBase.DateTime.MJD,
+                                  dafBase.DateTime.UTC).toString()[:-1]
+        localObs = mjdObs - 10.0/24.0
+        validStart = math.floor(localObs - 0.5) + 0.5 + 10.0/24.0 # Local midday, in UTC
+        validEnd = validStart + 1.0
+        validStart = dafBase.DateTime(validStart, dafBase.DateTime.MJD,
+                                      dafBase.DateTime.UTC).toString()[:-1]
+        validEnd = dafBase.DateTime(validEnd, dafBase.DateTime.MJD,
+                                    dafBase.DateTime.UTC).toString()[:-1]
+    else:
+        # Make data valid for (almost) all time
+        validStart = "2000-01-01"
+        validEnd = "2099-12-31"
+        taiObs = dateObs
 
     # print "%f --> %f --> %f %f" % (mjdObs, localObs, validStart, validEnd)
-
-    taiObs = dafBase.DateTime(mjdObs, dafBase.DateTime.MJD,
-            dafBase.DateTime.UTC).toString()[:-1]
-    validStart = dafBase.DateTime(validStart, dafBase.DateTime.MJD,
-                                  dafBase.DateTime.UTC).toString()[:-1]
-    validEnd = dafBase.DateTime(validEnd, dafBase.DateTime.MJD,
-                                dafBase.DateTime.UTC).toString()[:-1]
 
     derivedRunId = runId
 
