30a8fb565067d71db3a5ee7e17ca9486b13d3d69
price <price@git.lsstcorp.org>
2011-02-01 17:11:41 +0000

    Got it working with the trunk butler (daf_persistence + daf_butlerUtils).  Removed products we don't currently care about.

--- python/lsst/obs/suprime/suprimeMapper.py
+++ python/lsst/obs/suprime/suprimeMapper.py
@@ -1,28 +1,25 @@
 import os
 import re
 
-import lsst.daf.persistence as dafPersist
+from lsst.daf.butlerUtils import CameraMapper
+import lsst.pex.policy as pexPolicy
 import lsst.afw.image as afwImage
 import lsst.afw.cameraGeom as cameraGeom
 
-class SuprimeMapper(dafPersist.Mapper):
-    def __init__(self, policy=None, root=".", registry=None, calibRoot=None):
-        dafPersist.Mapper.__init__(self, policy="SuprimeMapper.paf", module="obs_subaru",
-                                   policyDir="policy", root=root, registry=registry, calibRoot=calibRoot)
+class SuprimeMapper(CameraMapper):
+    def __init__(self, **kwargs):
+        policyFile = pexPolicy.DefaultPolicyFile("obs_subaru", "SuprimeMapper.paf", "policy")
+        policy = pexPolicy.Policy(policyFile)
+        super(SuprimeMapper, self).__init__(policy, policyFile.getRepositoryPath(), **kwargs)
 
-        self.keys = ["field", "visit", "ccd", "amp", "filter", "mystery", "taiObs" ]
-        self.keys += ["filter", "expTime"]
-
-        self.filterKey = "FILTER01"
-        self.filterMap = {
+        self.filters = {
             "W-J-B"   : "B",
-            "W-S-R+"  : "i",
+            "W-S-G+"  : "g",
+            "W-S-R+"  : "r",
             "W-S-I+"  : "i",
             "W-S-Z+"  : "z",
             "W-S-ZR"  : "y",
             }
-
-        self.filterIdMap = { 'B': 1, 'u': 0, 'g': 1, 'r': 2, 'i': 3, 'z': 4, 'y': 5 }
 
     def _extractAmpId(self, dataId):
         return (self._extractDetectorName(dataId), 0, 0)
@@ -34,30 +31,14 @@ class SuprimeMapper(dafPersist.Mapper):
         ccdTmp = int("%(ccd)d" % dataId)
         return miyazakiNames[ccdTmp]
 
-    def _transformId(self, dataId):
-        return dataId.copy()
-
-    def standardize_raw(self, mapping, item, dataId):
-        dataId = self._transformId(dataId)
-        exposure = afwImage.makeExposure(
-            afwImage.makeMaskedImage(item.getImage()))
-        md = item.getMetadata()
-        exposure.setMetadata(md)
-        exposure.setWcs(afwImage.makeWcs(md))
-        wcsMetadata = exposure.getWcs().getFitsMetadata()
-        for kw in wcsMetadata.paramNames():
-            md.remove(kw)
-        exp = self._standardize(mapping, exposure, dataId)
-        gain = md.get('GAIN')
-        for amp in cameraGeom.cast_Ccd(exp.getDetector()):
-            amp.getElectronicParams().setGain(gain)
-        if dataId.has_key('filter'):
-            filterName = dataId['filter']
-        elif md.exists(self.filterKey):
-            filterName = item.getMetadata().get(self.filterKey).strip()
-            if self.filterMap.has_key(filterName):
-                filterName = self.filterMap[filterName]
-        else:
-            raise RuntimeError("Unable to determine filter name")
-        exp.setFilter(afwImage.Filter(filterName))
-        return exp
+### XXX Not necessary now that gains are in the camera configuration
+#    def std_raw(self, item, dataId):
+#        exposure = super(SuprimeMapper, self).std_raw(item, dataId)
+#
+#        # Set gains from the header
+#        md = exposure.getMetadata()
+#        gain = md.get('GAIN')
+#        for amp in cameraGeom.cast_Ccd(exposure.getDetector()):
+#            amp.getElectronicParams().setGain(gain)
+#            
+#        return exposure
