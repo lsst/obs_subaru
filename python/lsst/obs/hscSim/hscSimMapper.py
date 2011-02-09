7a524a28561cf71c1afc5e77eb60f2bcd696167d
price <price@git.lsstcorp.org>
2011-02-09 21:17:37 +0000

    Add HSC (simulated data) mapper.

---
+++ python/lsst/obs/hscSim/hscSimMapper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from lsst.daf.butlerUtils import CameraMapper
+import lsst.pex.policy as pexPolicy
+
+class HscSimMapper(CameraMapper):
+    """Provides abstract-physical mapping for HSC Simulation data"""
+    
+    def __init__(self, policy=None, root=".", registry=None, calibRoot=None):
+        policyFile = pexPolicy.DefaultPolicyFile("obs_subaru", "HscSimMapper.paf", "policy")
+        policy = pexPolicy.Policy(policyFile)
+        super(HscSimMapper, self).__init__(policy, policyFile.getRepositoryPath(), **kwargs)
+
+        self.filterss = {
+            "W-J-B"   : "B",
+            "W-S-G+"  : "g",
+            "W-S-R+"  : "r",
+            "W-S-I+"  : "i",
+            "W-S-Z+"  : "z",
+            "W-S-ZR"  : "y",
+            }
+
+    def _extractAmpId(self, dataId):
+        return (self._extractDetectorName(dataId), 0, 0)
+
+    def _extractDetectorName(self, dataId):
+        return int("%(ccd)d" % dataId)
+
