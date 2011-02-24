11ef2517761f65934c4e3af84fbbdb3c1882de70
Craig Loomis <cloomis@astro.princeton.edu>
2011-02-24 11:06:55 +0900

    fiddling

--- python/hsc/meas/match/hscDistortion.py
+++ python/hsc/meas/match/hscDistortion.py
@@ -1,3 +1,5 @@
+import lsst.afw.geom as afwGeom
+import lsst.afw.image as afwImage
 import lsst.pipette.distortion as pipDist
 import hsc.meas.match.distest as distest
 
@@ -17,6 +19,8 @@ class HscDistortion(pipDist.CameraDistortion):
         position = ccd.getCenter()        # Centre of CCD on focal plane
         center = ccd.getSize() / ccd.getPixelSize() / 2.0 # Central pixel
 
+        self.ccd = ccd
+
         # Pixels from focal plane center to CCD corner
         self.x0 = position.getX() - center.getX()
         self.y0 = position.getY() - center.getY()
@@ -29,8 +33,15 @@ class HscDistortion(pipDist.CameraDistortion):
         @param direction "toIdeal" or "toActual"
         @returns Copy of input source with distorted/undistorted coordinates
         """
-        x += self.x0
-        y += self.y0
+
+        if False:
+            point = afwGeom.makePointD(x, y)
+            point = self.ccd.getPositionFromPixel(point)
+            pixelSize = self.ccd.getPixelSize()
+            x, y = point.getX() / pixelSize, point.getY() / pixelSize
+        else:
+            x += self.x0
+            y += self.y0
 
         if direction == "toIdeal":
             distX, distY = distest.getUndistortedPosition(x, y, elevation)
@@ -39,7 +50,13 @@ class HscDistortion(pipDist.CameraDistortion):
         else:
             raise RuntimeError("unknown distortion direction: %s" % (direction))
 
-        return distX - self.x0, distY - self.y0
+        if False:
+            point.setX(distX * pixelSize)
+            point.setY(distY * pixelSize)
+            point = self.ccd.getPixelFromPosition(point)
+            return point.getX(), point.getY()
+        else:
+            return distX - self.x0, distY - self.y0
     
     # Need to get elevation in here -- CPL
     def actualToIdeal(self, sources, copy=True):
