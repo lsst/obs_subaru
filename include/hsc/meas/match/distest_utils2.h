d41ea8881d5bc5d16fd74683288b61d6ce99b347
Hisanori Furusawa <furusawa@subaru.naoj.org>
2011-04-08 18:09:29 +0900

    Enhanced: distest_utils2.cc: uses distortion model with 9th polynomials (as of 2010/11/08). Iterative transformation for getDistortedPosition is implemented. example & tests now work.

---
+++ include/hsc/meas/match/distest_utils2.h
@@ -0,0 +1,31 @@
+#ifndef DISTEST_UTILS2_H
+#define DISTEST_UTILS2_H
+#include<iostream>
+#include<cstdlib>
+#include<fstream>
+#include<cmath>
+
+#define XYOrder 9
+
+/* distortion coefficients by Okura-kun, to be accessible in any functions */
+//extern double ****Coef;
+
+//int initDistEst(double ****Coef);
+//int deleteDistEst(double ****Coef);
+void convUndist2DistPos(float x_undist, float y_undist, 
+                        float *x_dist, float *y_dist, float elevation, double ***Coef);
+void convUndist2DistPosIterative(float x_undist, float y_undist, 
+                                 float *x_dist, float *y_dist, float elevation, double ***Coef);
+void convDist2UndistPos(float x_dist, float y_dist, 
+                        float *x_undist, float *y_undist, float elevation, double ***Coef);
+void   F_SETCOEF(double ***Coef);
+//double F_CCDtoSKY_X(int, double, double ****, double, double);
+//double F_CCDtoSKY_Y(int, double, double ****, double, double);
+//double F_SKYtoCCD_X(int, double, double ****, double, double);
+//double F_SKYtoCCD_Y(int, double, double ****, double, double);
+//double F_CS(int CS,int ELOrder, double EL,double ****Coef, double X, double Y);
+int F_CS_CCD2SKY_XY(int ELOrder, double EL, double ***Coef, double X, double Y, double *X_out, double *Y_out);
+int F_CS_SKY2CCD_XY(int ELOrder, double EL, double ***Coef, double X, double Y, double *X_out, double *Y_out);
+///void   F_LS1(int ,int ,double **,double *);
+
+#endif
