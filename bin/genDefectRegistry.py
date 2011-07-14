7dc33f353fca3bb31a9ff752627e01c11784eb07
price <price@git.lsstcorp.org>
2011-07-14 18:53:58 +0000

    Copied from obs_lsstSim, since at the moment we don't need to worry about validity ranges.

--- bin/genDefectRegistry.py
+++ bin/genDefectRegistry.py
@@ -3,63 +3,28 @@
 import glob
 import os
 import re
-import sqlite3
+import sqlite
 import sys
 
 if os.path.exists("defectRegistry.sqlite3"):
     os.unlink("defectRegistry.sqlite3")
-conn = sqlite3.connect("defectRegistry.sqlite3")
+conn = sqlite.connect("defectRegistry.sqlite3")
 
 cmd = "create table defect (id integer primary key autoincrement"
-cmd += ", path text, version int, ccdSerial int"
+cmd += ", path text, version text, ccdSerial int"
 cmd += ", validStart text, validEnd text)"
 conn.execute(cmd)
 conn.commit()
 
-months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+cmd = "INSERT INTO defect VALUES (NULL, ?, ?, ?, ?, ?)"
 
-f = file(sys.argv[1])
-root = sys.argv[2]
-for line in f:
-    if line.startswith('#'):
+for f in glob.glob("*/defects*.fits"):
+    m = re.search(r'(\S+)/defects_(\d+)\.fits', f)
+    if not m:
+        print >>sys.stderr, "Unrecognized file: %s" % (f,)
         continue
-    words = line.split()
-    if len(words) == 0:
-        continue
-    elif len(words) != 8:
-        print "Warning: Unrecognized line:"
-        print line
-        continue
-    path, runId, start, stop, filter, detrend, version, ccds = \
-            line.split()
-
-    if detrend != "mask":
-        continue
-
-    y, m, d = start.split("-")
-    m = months.index(m) + 1
-    start = "%s-%02d-%s" % (y, m, d)
-
-    y, m, d = stop.split("-")
-    m = months.index(m) + 1
-    stop = "%s-%02d-%s" % (y, m, d)
-
-    cmd = "INSERT INTO defect VALUES (NULL, ?, ?, ?, ?, ?)"
-    pathwords = path.split(".")
-    if pathwords[0] == "static":
-        path = ".".join(pathwords[:5])
-    else:
-        path = ".".join(pathwords[:5]) + ".n"
-
-    for f in glob.glob(os.path.join(root, path, "defects*.fits")):
-        m = re.search(r'defects(\d+)\.fits', f)
-        if not m:
-            print >>sys.stderr, "Unrecognized file: %s" % (f,)
-            continue
-        print f
-        conn.execute(cmd, (f, version, int(m.group(1)), start, stop))
-
-    conn.commit()
-
+    print f
+    conn.execute(cmd, (f, m.group(1), int(m.group(2)), "1970-01-01", "2037-12-31"))
+conn.commit()
 conn.close()
+
