cheezily call an imshow wrapper, to get fixed sinh scaling

examples/plotDeblend.py
@@ -14,6 +14,31 @@ def setupPyplot():
     clippedCm.set_over(color='red')
     clippedCm.set_under(color='blue')
 
+def getImgScale(img, pmin=0.0, pmax=1.0):
+    ss = np.sort(img.ravel())
+    mn,mx = [ss[int(p*(len(ss)-1))] for p in [pmin, pmax]]
+    #print 'mn,mx', mn,mx
+
+    q1,q2,q3 = [ss[int(p*len(ss))] for p in [0.25, 0.5, 0.75]]
+
+    return mn, q1, q2, q3, mx
+
+def myimshow(imshow, x, qscales=None, *args, **kwargs):
+    mykwargs = kwargs.copy()
+
+    if qscales == None:
+        qscales = getImgScale(x, **mykwargs)
+    mn, q1, q2, q3, mx = qscales
+
+    def nlmap(X, q1, q2, q3):
+        Y = (X - q2) / ((q3-q1)/2.)
+        return np.arcsinh(Y * 10.)/10.
+
+    mykwargs['vmin'] = nlmap(mn, q1, q2, q3)
+    mykwargs['vmax'] = nlmap(mx, q1, q2, q3)
+
+    return imshow(nlmap(x, q1, q2, q3), *args, **mykwargs)
+
 def imgScale(img):
     flatImg = img.flatten()
     s = np.argsort(flatImg, axis=None)
@@ -31,7 +56,6 @@ def plotFootprint(exposure, psf, bb, bootPrint):
                 bb.getMinY(), bb.getMaxY())
     footxy0 = (bb.getMinX(), bb.getMinY())
     bootPeaks = bootPrint.peaks
-    vmin, vmax = imgScale(footImg)
 
     if bb.getWidth() >= bb.getHeight():
         nRow = 2
@@ -47,13 +71,13 @@ def plotFootprint(exposure, psf, bb, bootPrint):
                               cbar_mode='each',
                               axes_pad=0.1)
 
-
-    footprintGrid[0].imshow(footImg,
-                            interpolation='nearest',
-                            origin='lower',
-                            cmap=clippedCm,
-                            vmin=vmin, vmax=vmax,
-                            extent=bbExtent)
+    qscales = getImgScale(footImg)
+    myimshow(footprintGrid[0].imshow, footImg,
+             qscales=qscales,
+             interpolation='nearest',
+             origin='lower',
+             cmap=clippedCm,
+             extent=bbExtent)
     ax = footprintGrid[0].get_axes()
 
     nPlot = len(bootPeaks)+1            # input image and residuals
@@ -67,24 +91,26 @@ def plotFootprint(exposure, psf, bb, bootPrint):
                             share_all=True,
                             axes_pad=0.1)
 
-    deblendGrid[0].imshow(footImg,
-                          interpolation='nearest',
-                          origin='lower',
-                          cmap=clippedCm,
-                          aspect='equal',
-                          extent=bbExtent)
+    myimshow(deblendGrid[0].imshow, footImg,
+             qscales=qscales,
+             interpolation='nearest',
+             origin='lower',
+             cmap=clippedCm,
+             aspect='equal',
+             extent=bbExtent)
 
     for i in range(len(bootPeaks)):
         pk = bootPeaks[i]
         if pk.out_of_bounds:
             thisImg = residImg*0 - 1
-            deblendGrid[i+1].imshow(thisImg,
-                                    interpolation='nearest',
-                                    origin='lower',
-                                    cmap=clippedCm,
-                                    aspect='equal',
-                                    extent=bbExtent)
-            
+            myimshow(deblendGrid[i+1].imshow, thisImg,
+                     qscales=qscales,
+                     interpolation='nearest',
+                     origin='lower',
+                     cmap=clippedCm,
+                     aspect='equal',
+                     extent=bbExtent)
+
             continue
 
         stampImg = pk.portion.getArray()
@@ -97,24 +123,24 @@ def plotFootprint(exposure, psf, bb, bootPrint):
         #            pk.stampxy0[1] - footxy0[1])
         #stampxy1 = (pk.stampxy1[0] - footxy0[0],
         #            pk.stampxy1[1] - footxy0[1])
-        residImg[stampxy0[1]:stampxy1[1]+1,
-                 stampxy0[0]:stampxy1[0]+1] -= stampImg
-
         thisImg = residImg * 0 + np.median(stampImg)
-        thisImg[stampxy0[1]:stampxy1[1]+1,
-                stampxy0[0]:stampxy1[0]+1] = stampImg
+        try:
+            residImg[stampxy0[1]:stampxy1[1]+1,
+                     stampxy0[0]:stampxy1[0]+1] -= stampImg
 
-        extent = (pk.stampxy0[0],
-                  pk.stampxy1[0],
-                  pk.stampxy0[1],
-                  pk.stampxy1[1])
+            thisImg[stampxy0[1]:stampxy1[1]+1,
+                    stampxy0[0]:stampxy1[0]+1] = stampImg
+        except Exception, e:
+            print "subframe botch"
+            debug_here()
 
-        deblendGrid[i+1].imshow(thisImg,
-                                interpolation='nearest',
-                                origin='lower',
-                                cmap=clippedCm,
-                                aspect='equal',
-                                extent=bbExtent)
+        myimshow(deblendGrid[i+1].imshow, thisImg,
+                 qscales=qscales,
+                 interpolation='nearest',
+                 origin='lower',
+                 cmap=clippedCm,
+                 aspect='equal',
+                 extent=bbExtent)
 
         deblendGrid[i+1].plot(pk.center[0], pk.center[1], '+',
                               color='red' if pk.deblend_as_psf else 'blue',
@@ -131,12 +157,13 @@ def plotFootprint(exposure, psf, bb, bootPrint):
                               scalex=False, scaley=False)
 
 
-    footprintGrid[1].imshow(residImg,
-                            interpolation='nearest',
-                            origin='lower',
-                            cmap=clippedCm,
-                            aspect='equal',
-                            extent=bbExtent)
+    myimshow(footprintGrid[1].imshow, residImg,
+             qscales=qscales,
+             interpolation='nearest',
+             origin='lower',
+             cmap=clippedCm,
+             aspect='equal',
+             extent=bbExtent)
 
     pyplot.show()
     debug_here()