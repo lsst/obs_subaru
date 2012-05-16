17116c53abf4b599a32d97eebc881c62b95d72ad
Dustin Lang <dstn@astro.princeton.edu>
2012-05-16 16:57:13 -0400

    cosmetic changes to design.tex tables; add script to generate plots

---
+++ doc/plots.sh
@@ -0,0 +1,10 @@
+
+
+# SDSS section
+python examples/designdoc.py --drill 828 --order 1,0,2 --pdf
+
+# Monotonic section
+python examples/designdoc.py --drill 1173 --figh 2.5 --pdf
+python examples/designdoc.py --drill 1173 --figh 2.5 --mono --prefix design-mono- --pdf
+python examples/designdoc.py --drill 2439 --figw 3 --order 3,0,1,2 --pdf
+python examples/designdoc.py --drill 2439 --figw 3 --order 3,0,1,2 --mono --prefix design-mono- --pdf
