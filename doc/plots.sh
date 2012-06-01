aff0f55752c553fb27409ba8bd45608c07eb2694
Dustin Lang <dstn@astro.princeton.edu>
2012-06-01 12:13:55 -0400

    new figs, order plots

--- doc/plots.sh
+++ doc/plots.sh
@@ -13,10 +13,10 @@ python examples/designdoc.py ${ARGS} --drillxy 1665,1543 --order 1,0,2 --pat "de
 python examples/designdoc.py ${ARGS} --drillxy 1545,2131 --pat "design-mono1-%(name)s.pdf" --figh 2.5
 python examples/designdoc.py ${ARGS} --drillxy 1545,2131 --pat "design-mono2-%(name)s.pdf" --figh 2.5 --mono
 
-python examples/designdoc.py ${ARGS} --drillxy 1378,3971 --pat "design-mono3-%(name)s.pdf" --figw 3
-python examples/designdoc.py ${ARGS} --drillxy 1378,3971 --pat "design-mono4-%(name)s.pdf" --figw 3 --mono
+python examples/designdoc.py ${ARGS} --drillxy 1378,3971 --order 3,0,1,2 --pat "design-mono3-%(name)s.pdf" --figw 3
+python examples/designdoc.py ${ARGS} --drillxy 1378,3971 --order 3,0,1,2 --pat "design-mono4-%(name)s.pdf" --figw 3 --mono
 
 # Median-filter section
 python examples/designdoc.py ${ARGS} --drillxy 1545,2131 --pat "design-med1-%(name)s.pdf" --figh 2.5 --median
-python examples/designdoc.py ${ARGS} --drillxy 1378,3971 --pat "design-med2-%(name)s.pdf" --figw 3   --median
+python examples/designdoc.py ${ARGS} --drillxy 1378,3971 --order 3,0,1,2 --pat "design-med2-%(name)s.pdf" --figw 3   --median
 
