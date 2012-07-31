f54affb21b504ec2b3fcb5404bfe20b34f17ad5a
Paul Price <price@astro.princeton.edu>
2012-07-31 22:04:21 +0000

    Use SubaruIsrTask for hscSim.

--- config/hscSim/processCcd.py
+++ config/hscSim/processCcd.py
@@ -2,6 +2,11 @@
 HSC-specific overrides for ProcessCcdTask
 (applied after Subaru overrides in ../processCcd.py).
 """
+from lsst.obs.subaru.isr import SubaruIsrTask
+
+root.isr.retarget(SubaruIsrTask)
 root.isr.doBias = False
 root.isr.doDark = False
 root.isr.doWrite = False
+root.isr.doCrosstalk = False
+root.isr.doGuider = False
