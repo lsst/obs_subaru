fbf9adf643b42c1d9050233e5821c29a6a9195d0
Paul Price <price@astro.princeton.edu>
2012-11-27 22:52:31 -0500

    Move ISR configuration to separate file.  Add configuration for new detrend construction tasks.

---
+++ config/suprimecam/isr.py
@@ -0,0 +1,19 @@
+# Configuration for Suprime-Cam ISR
+
+from lsst.obs.subaru.isr import SuprimeCamIsrTask
+
+root.isr.retarget(SuprimeCamIsrTask)  # custom task that adds guider correction
+root.isr.doBias = False
+root.isr.doDark = False
+root.isr.doWrite = False
+
+# crosstalk coefficients for SuprimeCam, as crudely measured by RHL
+root.isr.crosstalkCoeffs.values = [
+     0.00e+00, -8.93e-05, -1.11e-04, -1.18e-04,
+    -8.09e-05,  0.00e+00, -7.15e-06, -1.12e-04,
+    -9.90e-05, -2.28e-05,  0.00e+00, -9.64e-05,
+    -9.59e-05, -9.85e-05, -8.77e-05,  0.00e+00,
+    ]
+
+# nonlinearity for SuprimeCam
+root.isr.linearizationCoefficient = 2.5e-7
--- config/suprimecam/processCcd.py
+++ config/suprimecam/processCcd.py
@@ -2,23 +2,8 @@
 SuprimeCam-specific overrides for ProcessCcdTask
 (applied after Subaru overrides in ../processCcd.py).
 """
-from lsst.obs.subaru.isr import SuprimeCamIsrTask
-
-root.isr.retarget(SuprimeCamIsrTask)  # custom task that adds guider correction
-root.isr.doBias = False
-root.isr.doDark = False
-root.isr.doWrite = False
-
-# crosstalk coefficients for SuprimeCam, as crudely measured by RHL
-root.isr.crosstalkCoeffs.values = [
-     0.00e+00, -8.93e-05, -1.11e-04, -1.18e-04,
-    -8.09e-05,  0.00e+00, -7.15e-06, -1.12e-04,
-    -9.90e-05, -2.28e-05,  0.00e+00, -9.64e-05,
-    -9.59e-05, -9.85e-05, -8.77e-05,  0.00e+00,
-    ]
-
-# nonlinearity for SuprimeCam
-root.isr.linearizationCoefficient = 2.5e-7
+import os
+root.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'suprimecam', 'isr.py'))
 
 # color terms
 from lsst.meas.photocal.colorterms import Colorterm
