c7fd9a94c1f687719fd86dfbd13fa6c6b0a7b1b5
Paul Price <price@astro.princeton.edu>
2012-12-07 10:53:35 -0500

    Add skymap configuration

---
+++ config/makeSkyMap.py
@@ -0,0 +1,6 @@
+root.skyMap = "healpix"
+root.skyMap["healpix"].log2NSide = 5
+root.skyMap["healpix"].pixelScale = 0.18
+root.skyMap["healpix"].patchBorder = 333 # Pixels
+root.skyMap["healpix"].tractOverlap = 2.0/60 # Degrees
+root.skyMap["healpix"].projection = "TAN"
