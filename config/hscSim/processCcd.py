2d47e4d714e7878c97013adfaecb094fe6f4467e
Paul Price <price@astro.princeton.edu>
2013-01-31 20:33:33 -1000

    Add configuration files for HSC ISR (both with processCcd and detrend construction).

--- config/hscSim/processCcd.py
+++ config/hscSim/processCcd.py
@@ -2,14 +2,9 @@
 HSC-specific overrides for ProcessCcdTask
 (applied after Subaru overrides in ../processCcd.py).
 """
-from lsst.obs.subaru.isr import SubaruIsrTask
 
-root.isr.retarget(SubaruIsrTask)
-root.isr.doBias = False
-root.isr.doDark = False
-root.isr.doWrite = False
-root.isr.doCrosstalk = False
-root.isr.doGuider = False
+import os
+root.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'hscSim', 'isr.py'))
 
 root.calibrate.measurePsf.starSelector.name = "secondMoment" # "objectSize" has problems with corner CCDs
 
