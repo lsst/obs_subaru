1d338413733fbda21de31ca6c26a3a48bb70ff4b
Robert Lupton the Good <rhl@astro.princeton.edu>
2013-02-26 13:17:29 +0900

    Add --summary option

--- bin/registryInfo.py
+++ bin/registryInfo.py
@@ -14,6 +14,7 @@ If no registry is provided, try $SUPRIME_DATA_DIR
     parser.add_argument('registryFile', type=str, nargs="?", help="The registry or directory in question")
     parser.add_argument('--field', type=str, help="Just tell me about this field (may be a glob with *)")
     parser.add_argument('--visit', type=int, help="Just tell me about this visit")
+    parser.add_argument('-s', '--summary', action="store_true", help="Print summary (grouped by field)")
     
     args = parser.parse_args()
 
@@ -30,7 +31,10 @@ If no registry is provided, try $SUPRIME_DATA_DIR
 
     conn = sqlite.connect(registryFile)
     cursor = conn.cursor()
-    print "%-7s %-20s %6s %7s %3s" % ("filter", "field", "visit", "expTime", "nCCD")
+    if args.summary:
+        print "%-7s %-20s %7s" % ("filter", "field", "expTime")
+    else:
+        print "%-7s %-20s %6s %7s %3s" % ("filter", "field", "visit", "expTime", "nCCD")
 
     where = []; vals = []
     if args.field:
@@ -50,9 +54,25 @@ GROUP BY visit
 ORDER BY filter
 """ % (where)
 
+    n = {}
+    expTimes = {}
     for line in cursor.execute(query, vals):
         field, visit, filter, expTime, nCCD = line
 
-        print "%-7s %-20s %6d %7.1f %3d" % (filter, field, visit, expTime, nCCD)
+        if args.summary:
+            k = (filter, field)
+            if not n.get(k):
+                n[k] = 0
+                expTimes[k] = 0
+
+            n[k] += 1
+            expTimes[k] += expTime
+        else:
+            print "%-7s %-20s %6d %7.1f %3d" % (filter, field, visit, expTime, nCCD)
 
     conn.close()
+
+    if args.summary:
+        for k in sorted(n.keys()):
+            filter, field = k
+            print "%-7s %-20s %7.1f" % (filter, field, expTimes[k])
