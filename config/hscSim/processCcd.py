266eb7abb79a16abf4756c4e74ceb5a071d8c679
Paul Price <price@astro.princeton.edu>
2013-03-01 15:30:00 +0900

    Add Yasuda-san's suggested parameters for astrometry with HSC.

--- config/hscSim/processCcd.py
+++ config/hscSim/processCcd.py
@@ -8,3 +8,7 @@ root.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'hscSim', 'isr.py
 
 root.calibrate.measurePsf.starSelector.name = "secondMoment" # "objectSize" has problems with corner CCDs
 
+root.calibrate.astrometry.solver.sipOrder = 3
+root.calibrate.astrometry.solver.catalogMatchDist = 2.0
+root.calibrate.astrometry.solver.numBrightStars =  55
+root.calibrate.astrometry.solver.rotationAllowedInRad = 0.01
