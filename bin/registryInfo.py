cfa00b1d3209c21a2daca48cf62c53f5128dbb25
Robert Lupton the Good <rhl@astro.princeton.edu>
2013-03-06 04:55:56 +0900

    Added ability to query calib registries

--- bin/registryInfo.py
+++ bin/registryInfo.py
@@ -43,45 +43,19 @@ def formatVisits(visits):
 
     return "^".join(visitSummary)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="""
-Dump the contents of a registry
-
-If no registry is provided, try $SUPRIME_DATA_DIR
-""")
-
-    parser.add_argument('registryFile', type=str, nargs="?", help="The registry or directory in question")
-    parser.add_argument('--field', type=str, help="Just tell me about this field (may be a glob with *)")
-    parser.add_argument('--visit', type=int, help="Just tell me about this visit")
-    parser.add_argument('-s', '--summary', action="store_true", help="Print summary (grouped by field)")
     
-    args = parser.parse_args()
-
-    if not args.registryFile:
-        args.registryFile = os.environ.get("SUPRIME_DATA_DIR", "")
-
-    if os.path.exists(args.registryFile) and not os.path.isdir(args.registryFile):
-        registryFile = args.registryFile
-    else:
-        registryFile = os.path.join(args.registryFile, "registry.sqlite3")
-        if not os.path.exists(registryFile):
-            print >> sys.stderr, "Unable to open %s" % args.registryFile
-            sys.exit(1)
-
-    conn = sqlite.connect(registryFile)
-    cursor = conn.cursor()
-    if args.summary:
-        print "%-7s %-20s %7s %s" % ("filter", "field", "expTime", "visit")
-    else:
-        print "%-7s %-20s %7s %6s %3s" % ("filter", "field", "expTime", "visit", "nCCD")
-
+def queryRegistry(field=None, visit=None, filterName=None, summary=False):
+    """Query an input registry"""
     where = []; vals = []
-    if args.field:
+    if field:
         where.append('field like ?')
-        vals.append(args.field.replace("*", "%"))
-    if args.visit:
+        vals.append(field.replace("*", "%"))
+    if filterName:
+        where.append('filter like ?')
+        vals.append(filterName.replace("*", "%"))
+    if visit:
         where.append("visit = ?")
-        vals.append(args.visit)
+        vals.append(visit)
     if where:
         where = "WHERE " + " AND ".join(where) if where else ""
 
@@ -94,10 +68,19 @@ ORDER BY filter
 """ % (where)
 
     n = {}; expTimes = {}; visits = {}
+
+    conn = sqlite.connect(registryFile)
+    cursor = conn.cursor()
+
+    if args.summary:
+        print "%-7s %-20s %7s %s" % ("filter", "field", "expTime", "visit")
+    else:
+        print "%-7s %-20s %7s %6s %3s" % ("filter", "field", "expTime", "visit", "nCCD")
+
     for line in cursor.execute(query, vals):
         field, visit, filter, expTime, nCCD = line
 
-        if args.summary:
+        if summary:
             k = (filter, field)
             if not n.get(k):
                 n[k] = 0
@@ -112,8 +95,88 @@ ORDER BY filter
 
     conn.close()
 
-    if args.summary:
+    if summary:
         for k in sorted(n.keys()):
             filter, field = k
 
             print "%-7s %-20s %7.1f %s" % (filter, field, expTimes[k], formatVisits(visits[k]))
+
+def queryCalibRegistry(what, filterName=None, summary=False):
+    """Query a calib registry"""
+    where = []; vals = []
+
+    if filterName:
+        where.append('filter like ?')
+        vals.append(filterName.replace("*", "%"))
+    if where:
+        where = "WHERE " + " AND ".join(where) if where else ""
+
+    query = """
+SELECT
+    validStart, validEnd, calibDate, filter, calibVersion, count(ccd)
+FROM %s
+%s
+ORDER BY filter
+""" % (what, where)
+
+    n = {}; expTimes = {}; visits = {}
+
+    conn = sqlite.connect(registryFile)
+    cursor = conn.cursor()
+
+    if summary:
+        print >> sys.stderr, "No summary is available for calib data"
+        return
+    else:
+        print "%-10s--%-10s  %-10s  %7s %12s %4s" % (
+            "validStart", "validEnd", "calibDate", "filter", "calibVersion", "nCCD")
+
+    for line in cursor.execute(query, vals):
+        validStart, validEnd, calibDate, filter, calibVersion, nCCD = line
+
+        print "%-10s--%-10s  %-10s  %7s %12s %4d" % (
+            validStart, validEnd, calibDate, filter, calibVersion, nCCD)
+
+    conn.close()
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="""
+Dump the contents of a registry
+
+If no registry is provided, try $SUPRIME_DATA_DIR
+""")
+
+    parser.add_argument('registryFile', type=str, nargs="?", help="The registry or directory in question")
+    parser.add_argument('--calib', type=str, help="The registration is a Calib; value is desired product")
+    parser.add_argument('--field', type=str, help="Just tell me about this field (may be a glob with *)")
+    parser.add_argument('--filter', dest="filterName", type=str,
+                        help="Just tell me about this filter (may be a glob with *)")
+    parser.add_argument('--verbose', action="store_true", help="How chatty should I be?", default=0)
+    parser.add_argument('--visit', type=int, help="Just tell me about this visit")
+    parser.add_argument('-s', '--summary', action="store_true", help="Print summary (grouped by field)")
+    
+    args = parser.parse_args()
+
+    if not args.registryFile:
+        args.registryFile = os.environ.get("SUPRIME_DATA_DIR", "")
+        if args.calib:
+            args.registryFile = os.path.join(args.registryFile, "CALIB")
+
+    if os.path.exists(args.registryFile) and not os.path.isdir(args.registryFile):
+        registryFile = args.registryFile
+    else:
+        registryFile = os.path.join(args.registryFile,
+                                    "calibRegistry.sqlite3" if args.calib else "registry.sqlite3")
+        if not os.path.exists(registryFile):
+            print >> sys.stderr, "Unable to open %s" % registryFile
+            sys.exit(1)
+
+    if args.verbose:
+        print "Reading %s" % registryFile
+
+    if args.calib:
+        queryCalibRegistry(args.calib, args.filterName, args.summary)
+    else:
+        queryRegistry(args.field, args.visit, args.filterName, args.summary)
