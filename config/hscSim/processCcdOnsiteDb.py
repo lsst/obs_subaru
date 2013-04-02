c24bf837921a5a943e303b35d35490494ba92879
Steven Bickerton <bick@astro.princeton.edu>
2013-04-02 17:27:12 +0900

    changed __file__ to os.path.join(os.environ['OBS_SUBARU_DIR'], etc) as __file__ fails in eval.

--- config/hscSim/processCcdOnsiteDb.py
+++ config/hscSim/processCcdOnsiteDb.py
@@ -3,4 +3,4 @@ root.onsiteDb.retarget(HscOnsiteDbTask)
 
 # Load regular processCcd configuration
 import os
-root.load(os.path.join(os.path.abspath(__file__), 'processCcd.py'))
+root.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'hscSim', 'processCcd.py'))
