60a0db20bc7ca89b7104de2286b36341ee864967
Paul Price <price@astro.princeton.edu>
2013-04-09 13:35:48 -0400

    Set measured x,y for vignetting pattern center.

--- config/hscSim/flat.py
+++ config/hscSim/flat.py
@@ -5,3 +5,5 @@ from lsst.obs.hscSim.detrends import HscFlatCombineTask
 root.combination.retarget(HscFlatCombineTask)
 root.combination.badAmpCcdList = [0]
 root.combination.badAmpList =    [2]
+root.combination.xCenter = -30
+root.combination.yCenter = -300
