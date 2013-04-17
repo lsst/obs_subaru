189cca4acc429cd9b2e6a25a04155bd5ffd7ce1c
Paul Price <price@astro.princeton.edu>
2013-04-17 22:46:04 +0000

    config: Retarget before overriding parameters.
    Otherwise, the overrides don't take effect.

--- config/hscSim/processExposure.py
+++ config/hscSim/processExposure.py
@@ -1,7 +1,7 @@
-import os
-root.processCcd.load(os.path.join(os.environ["OBS_SUBARU_DIR"], "config", "hscSim", "processCcd.py"))
-
 from lsst.obs.hscSim.processCcd import HscProcessCcdTask
 root.processCcd.retarget(HscProcessCcdTask)
+
+import os
+root.processCcd.load(os.path.join(os.environ["OBS_SUBARU_DIR"], "config", "hscSim", "processCcd.py"))
 
 root.instrument = "hsc"
