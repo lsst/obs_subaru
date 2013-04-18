6909d99b066bd4f55452b07df9cc3b68446bc5ee
Paul Price <price@astro.princeton.edu>
2013-04-18 10:34:31 -0400

    config: reload parameters after retarget
    Retargeting the processCcd Task in config/hscSim/processExposure.py
    has the effect of destroying all the parameter settings previously
    loaded in config/processExposure.py (and config/processCcd.py), so
    we need to reload them.

--- config/hscSim/processExposure.py
+++ config/hscSim/processExposure.py
@@ -1,7 +1,9 @@
 from lsst.obs.hscSim.processCcd import HscProcessCcdTask
-root.processCcd.retarget(HscProcessCcdTask)
+root.processCcd.retarget(HscProcessCcdTask) # This destroys previous parameter settings; need to reload
+
 
 import os
+root.processCcd.load(os.path.join(os.environ["OBS_SUBARU_DIR"], "config", "processCcd.py"))
 root.processCcd.load(os.path.join(os.environ["OBS_SUBARU_DIR"], "config", "hscSim", "processCcd.py"))
 
 root.instrument = "hsc"
