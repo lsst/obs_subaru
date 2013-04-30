c0ba664dd3e622de3bf2e9e7ccca8dd94b0e4c94
Paul Price <price@astro.princeton.edu>
2013-04-30 10:00:55 -0400

    config: disable solvetansip for SC-MIT
    solvetansip currently appears to produce incorrect output for SC-MIT,
    so need to turn it off for now.

--- config/suprimecam-mit/processExposure.py
+++ config/suprimecam-mit/processExposure.py
@@ -2,4 +2,4 @@ import os
 root.processCcd.load(os.path.join(os.environ["OBS_SUBARU_DIR"], "config", "suprimecam-mit", "processCcd.py"))
 
 root.instrument = "suprimecam-mit"
-
+root.doSolveTansip = False
