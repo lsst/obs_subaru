cde1055e58c464ea78584dff505f3be813392a4f
Paul Price <price@astro.princeton.edu>
2013-05-01 12:15:53 -0400

    config: reactivate solvetansip for SC-MIT
    Okura-san has fixed solvetansip (5.2.1) to allow it to work on SC-MIT.

--- config/suprimecam-mit/processExposure.py
+++ config/suprimecam-mit/processExposure.py
@@ -2,4 +2,3 @@ import os
 root.processCcd.load(os.path.join(os.environ["OBS_SUBARU_DIR"], "config", "suprimecam-mit", "processCcd.py"))
 
 root.instrument = "suprimecam-mit"
-root.doSolveTansip = False
