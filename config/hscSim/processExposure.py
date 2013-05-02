6d08717d38fba5345b2215fb786e4a0be8e1576a
Paul Price <price@astro.princeton.edu>
2013-05-02 16:18:21 -0400

    HSC: move HscProcessCcdTask into hscPipe's SubaruProcessCcd
    The functionality to ignore particular CCDs is generally useful.

--- config/hscSim/processExposure.py
+++ config/hscSim/processExposure.py
@@ -1,9 +1,5 @@
-from lsst.obs.hscSim.processCcd import HscProcessCcdTask
-root.processCcd.retarget(HscProcessCcdTask) # This destroys previous parameter settings; need to reload
-
-
 import os
-root.processCcd.load(os.path.join(os.environ["OBS_SUBARU_DIR"], "config", "processCcd.py"))
 root.processCcd.load(os.path.join(os.environ["OBS_SUBARU_DIR"], "config", "hscSim", "processCcd.py"))
 
 root.instrument = "hsc"
+root.processCcd.ignoreCcdList = range(104, 112) # Focus/guide CCDs
