3d6c9d3e4ccbbd4d6c9ae39cafc59c2364c904ea
Paul Price <price@astro.princeton.edu>
2013-05-02 16:19:40 -0400

    SC-MIT: Ignore CCD 0 in ProcessExposureTask
    CCD 0 has low QE and bad CTE; we don't want to bother processing it.

--- config/suprimecam-mit/processExposure.py
+++ config/suprimecam-mit/processExposure.py
@@ -2,3 +2,4 @@ import os
 root.processCcd.load(os.path.join(os.environ["OBS_SUBARU_DIR"], "config", "suprimecam-mit", "processCcd.py"))
 
 root.instrument = "suprimecam-mit"
+root.processCcd.ignoreCcdList = [0] # Low QE, bad CTE
