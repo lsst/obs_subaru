a04213dee1677c89b0de72d949bbd9c3a44ffb19
Dustin Lang <dstndstn@gmail.com>
2013-06-06 14:55:22 -0400

    add script to just re-run the deblending phase, rather than starting from scratch every time

---
+++ examples/rerun.py
@@ -0,0 +1,33 @@
+from lsst.pex.config import Config, ConfigurableField
+from lsst.pipe.base import CmdLineTask
+from lsst.meas.algorithms import SourceDeblendTask
+import lsst.afw.table as afwTable
+
+class MyConfig(Config):
+    deblend = ConfigurableField(target=SourceDeblendTask, doc="Deblender")
+
+class MyTask(CmdLineTask):
+    _DefaultName = "my"
+    ConfigClass = MyConfig
+
+    def _getConfigName(self):
+        return None
+
+    def __init__(self, *args, **kwargs):
+        super(MyTask, self).__init__(*args, **kwargs)
+        # Don't really care about schema because the sources we read already have that,
+        # but the SourceDeblendTask constructor requires it.
+        self.schema = kwargs.pop("schema", None)
+        if self.schema is None:
+            self.schema = afwTable.SourceTable.makeMinimalSchema()
+        self.makeSubtask("deblend", schema=self.schema)
+
+    def run(self, dataRef):
+        calexp = dataRef.get("calexp")
+        psf = calexp.getPsf()
+        sources = dataRef.get("src")
+
+        self.deblend.run(calexp, sources, psf)
+
+if __name__ == "__main__":
+    MyTask.parseAndRun()
