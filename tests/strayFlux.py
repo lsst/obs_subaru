b9cecf5691519a7528c25ccca6daa61b0c8b629e
Dustin Lang <dstndstn@gmail.com>
2013-06-13 10:06:52 -0400

    plot the true image rather than the 'heavy'

--- tests/strayFlux.py
+++ tests/strayFlux.py
@@ -24,6 +24,10 @@ import lsst.meas.algorithms as measAlg
 root = pexLogging.Log.getDefaultLog()
 root.setThreshold(pexLogging.Log.DEBUG)
 
+# Quiet some of the more chatty loggers
+log = pexLogging.Log(root, 'lsst.meas.deblender.symmetrizeFootprint',
+                     pexLogging.Log.INFO)
+
 def imExt(img):
     bbox = img.getBBox(afwImage.PARENT)
     return [bbox.getMinX(), bbox.getMaxX(),
@@ -63,7 +67,7 @@ class StrayFluxTestCase(unittest.TestCase):
         fakepsf = gaussianPsf(11, 11, fakepsf_fwhm)
 
         blobimgs = []
-        XY = [(90.,50.), (50.,50.)]
+        XY = [(50.,50.), (90.,50.)]
         flux = 1e6
         for x,y in XY:
             bim = blob_psf.computeImage(afwGeom.Point2D(x, y))
@@ -99,7 +103,7 @@ class StrayFluxTestCase(unittest.TestCase):
         self.assertEqual(len(fp.getPeaks()), 2)
         
         ima = dict(interpolation='nearest', origin='lower', cmap='gray',
-                   vmin=0, vmax=1e2)
+                   vmin=0, vmax=100)
 
         if plots:
             plt.figure(figsize=(12,6))
@@ -130,7 +134,10 @@ class StrayFluxTestCase(unittest.TestCase):
 
         if plots:
             def myimshow(*args, **kwargs):
-                plt.imshow(*args, **kwargs)
+                x0,x1,y0,y1 = imExt(afwimg)
+                plt.fill([x0,x0,x1,x1,x0],[y0,y1,y1,y0,y0], color=(1,1,0.8),
+                         zorder=20)
+                plt.imshow(*args, zorder=25, **kwargs)
                 plt.xticks([]); plt.yticks([])
                 plt.axis(imExt(afwimg))
     
@@ -154,20 +161,24 @@ class StrayFluxTestCase(unittest.TestCase):
             
             sumimg = None
             for i,dpk in enumerate(deb.peaks):
+
                 plt.subplot(R, C, i*C + 1)
+                myimshow(blobimgs[i], **ima)
+                plt.title('true')
+
+                plt.subplot(R, C, i*C + 2)
                 myimshow(dpk.symm.getArray(), extent=imExt(dpk.symm), **ima)
                 plt.title('symm')
     
-                plt.subplot(R, C, i*C + 2)
+                plt.subplot(R, C, i*C + 3)
                 myimshow(dpk.portion.getArray(), extent=imExt(dpk.portion), **ima)
                 plt.title('portion')
-    
-                himg = afwImage.ImageF(fpbb)
-                dpk.heavy1.insert(himg)
-                
-                plt.subplot(R, C, i*C + 3)
-                myimshow(himg.getArray(), **ima)
-                plt.title('heavy')
+
+                # himg = afwImage.ImageF(fpbb)
+                # dpk.heavy1.insert(himg)
+                # plt.subplot(R, C, i*C + 3)
+                # myimshow(himg.getArray(), **ima)
+                # plt.title('heavy')
                 ax = plt.axis()
                 plt.plot([x for x,y in XY], [y for x,y in XY], 'r.')
                 plt.axis(ax)
