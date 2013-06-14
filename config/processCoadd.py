8769aba78f05a788147bd1b38e59929390f1fc0c
Paul Price <price@astro.princeton.edu>
2013-06-14 10:06:28 +0900

    config: add configuration for processCoadd

---
+++ config/processCoadd.py
@@ -0,0 +1,21 @@
+# Add another aperture flux - it shouldn't be this painful to do (see LSST #2465)
+from lsst.meas.algorithms.algorithmRegistry import AlgorithmRegistry, SincFluxConfig
+AlgorithmRegistry.register("flux.sinc2", target=SincFluxConfig.Control, ConfigClass=SincFluxConfig)
+root.measurement.algorithms["flux.sinc2"].radius = 5.0
+root.measurement.algorithms.names |= ["flux.sinc2"]
+
+root.measurement.slots.instFlux = None
+
+try:
+    import lsst.meas.extensions.photometryKron
+    root.measurement.algorithms.names |= ["flux.kron"]
+except ImportError:
+    print "Unable to import lsst.meas.extensions.photometryKron: Kron fluxes disabled"
+
+try:
+    import lsst.meas.extensions.multiShapelet
+    root.measurement.algorithms.names |= lsst.meas.extensions.multiShapelet.algorithms
+    root.measurement.slots.modelFlux = "multishapelet.combo.flux"
+except ImportError:
+    print "meas_extensions_multiShapelet is not setup; disabling model mags"
+
