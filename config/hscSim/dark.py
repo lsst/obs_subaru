30e37cc998a42685cfd6f14e06db12ddb66602ef
Paul Price <price@astro.princeton.edu>
2013-11-21 15:57:35 -0500

    hsc: add cosmic-ray removal options to dark construction

--- config/hscSim/dark.py
+++ config/hscSim/dark.py
@@ -2,3 +2,8 @@ import os
 root.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'hscSim', 'isr.py'))
 
 root.darkTime = None
+
+root.isr.doBias = True
+root.repair.cosmicray.nCrPixelMax = 1000000
+root.repair.cosmicray.minSigma = 5.0
+root.repair.cosmicray.min_DN = 50.0
