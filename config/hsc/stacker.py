5d64331adffd41bc66b6bfd41fe46c504f830faa
Paul Price <price@astro.princeton.edu>
2013-12-07 12:56:05 -0500

    Clean up after merging hscSim deprecation

--- config/hsc/stacker.py
+++ config/hsc/stacker.py
@@ -1,6 +1,6 @@
 # Load from sub-configurations
 import os
 for sub in ("makeCoaddTempExp", "backgroundReference", "assembleCoadd", "processCoadd"):
-    path = os.path.join(os.environ["OBS_SUBARU_DIR"], "config", "hscSim", sub + ".py")
+    path = os.path.join(os.environ["OBS_SUBARU_DIR"], "config", "hsc", sub + ".py")
     if os.path.exists(path):
         getattr(root, sub).load(path)
