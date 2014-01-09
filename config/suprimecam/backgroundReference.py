e8dc7b405d300534d466b8cf687813c20c5aee4e
Paul Price <price@astro.princeton.edu>
2014-01-09 19:54:29 +0000

    config: fix backgroundReference overrides for Suprime-Cam
    Was referring to the wrong config.

--- config/suprimecam/backgroundReference.py
+++ config/suprimecam/backgroundReference.py
@@ -1,7 +1,6 @@
 from hsc.pipe.tasks.background import XyTaperWeightImage
-bgRef = root.backgroundReference
-bgRef.construct.taper.retarget(XyTaperWeightImage)
-bgRef.construct.taper.xStart = 3000
-bgRef.construct.taper.xStop = 4750
-bgRef.construct.taper.yStart = 2500
-bgRef.construct.taper.yStop = 3750
+root.construct.taper.retarget(XyTaperWeightImage)
+root.construct.taper.xStart = 3000
+root.construct.taper.xStop = 4750
+root.construct.taper.yStart = 2500
+root.construct.taper.yStop = 3750
