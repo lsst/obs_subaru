268d6dfb0fa1bc82e9e727abd452822b086650ba
Paul Price <price@astro.princeton.edu>
2014-03-11 14:19:45 -1000

    config: set parameters for meas_mosaic on HSC
    Parameters courtesy of Yasuda-san.

--- config/hsc/Mosaic.py
+++ config/hsc/Mosaic.py
@@ -1,2 +1,9 @@
 import os
 root.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'hsc', 'colorterms.py'))
+
+root.fittingOrder = 9
+root.fluxFitOrder = 7
+root.fluxFitAbsolute = True
+root.internalFitting = True
+root.commonFluxCorr = False
+root.minNumMatch = 5
