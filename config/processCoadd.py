29a0383f49e14fbc1f5376197b8cba1daba7f545
Paul Price <price@astro.princeton.edu>
2014-03-12 10:33:28 -1000

    config: add series of apertures for processCoadd.

--- config/processCoadd.py
+++ config/processCoadd.py
@@ -5,6 +5,11 @@ if False: # This doesn't pickle
     root.measurement.algorithms["flux.sinc2"].radius = 5.0
     root.measurement.algorithms.names |= ["flux.sinc2"]
 
+root.measurement.algorithms.names |= ["flux.aperture"]
+# Roughly (1.0, 1.4, 2.0, 2.8, 4.0, 5.7, 8.0, 11.3, 16.0, 22.6 arcsec) in diameter: 2**(0.5*i)
+# (assumes 0.168 arcsec pixels on coadd)
+root.measurement.algorithms["flux.aperture"].radii = [3.0, 4.5, 6.0, 9.0, 12.0, 17.0, 25.0, 35.0, 50.0, 70.0]
+
 root.measurement.slots.instFlux = None
 
 try:
