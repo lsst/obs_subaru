34b52c36a129009428d143f0f7d28eb0797334e0
Paul Price <price@astro.princeton.edu>
2014-03-31 12:08:37 -0400

    config: remove HSC bad amp
    It's better now.

--- config/hsc/flat.py
+++ config/hsc/flat.py
@@ -3,8 +3,6 @@ root.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'hsc', 'isr.py'))
 
 from lsst.obs.hsc.detrends import HscFlatCombineTask
 root.combination.retarget(HscFlatCombineTask)
-root.combination.badAmpCcdList = [0]
-root.combination.badAmpList =    [2]
 root.combination.xCenter = -6.22
 root.combination.yCenter = -113.26
 root.combination.radius = 18000
