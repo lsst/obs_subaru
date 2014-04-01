eb19197f0477a2308b79ce2f2af7f90272cde2a9
Paul Price <price@astro.princeton.edu>
2014-03-31 22:26:44 -0400

    config: Tweak vignetting radius,center to reduce garbage at edges

--- config/hsc/flat.py
+++ config/hsc/flat.py
@@ -3,6 +3,6 @@ root.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'hsc', 'isr.py'))
 
 from lsst.obs.hsc.detrends import HscFlatCombineTask
 root.combination.retarget(HscFlatCombineTask)
-root.combination.xCenter = -6.22
-root.combination.yCenter = -113.26
-root.combination.radius = 18000
+root.combination.xCenter = -100
+root.combination.yCenter = 100
+root.combination.radius = 17500
