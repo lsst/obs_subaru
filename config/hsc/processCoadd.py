e08fe9d11465beb0a09a6f2899ef2f618ec6f2af
Paul Price <price@astro.princeton.edu>
2014-04-01 14:59:10 -0400

    config: consistent background subtraction for processCoadd

--- config/hsc/processCoadd.py
+++ config/hsc/processCoadd.py
@@ -5,3 +5,12 @@ HSC-specific overrides for ProcessCoaddTask
 
 import os
 root.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'hsc', 'colorterms.py'))
+
+bgSize = 4096
+bgUndersample = 'REDUCE_INTERP_ORDER'
+root.calibrate.background.binSize = bgSize
+root.calibrate.background.undersampleStyle = bgUndersample
+root.calibrate.detection.background.binSize = bgSize
+root.calibrate.detection.background.undersampleStyle = bgUndersample
+root.detection.background.binSize = bgSize
+root.detection.background.undersampleStyle = bgUndersample
