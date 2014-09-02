96e6546990cd05ba0973aedba51cb1814d1308b0
Paul Price <price@astro.princeton.edu>
2014-09-02 19:56:21 -0400

    config: factor out aperture settings, Kron, CModel activation
    Also sets forcedPhotCcd apertures to the standard set.
    
    Conflicts:
    	config/forcedPhotCcd.py
    	config/forcedPhotCoadd.py
    	config/processCcd.py
    	config/processCoadd.py

--- config/processCcd.py
+++ config/processCcd.py
@@ -2,6 +2,8 @@
 Subaru-specific overrides for ProcessCcdTask (applied before SuprimeCam- and HSC-specific overrides).
 """
 
+import os
+
 # This was a horrible choice of defaults: only the scaling of the flats
 # should determine the relative normalisations of the CCDs!
 root.isr.assembleCcd.doRenorm = False
@@ -54,16 +56,23 @@ root.detection.returnOriginalFootprints = False
 # Measurement
 root.doWriteSourceMatches = True
 
-root.measurement.algorithms.names |= ['base_CircularApertureFlux']
-# Roughly (1.0, 1.4, 2.0, 2.8, 4.0, 5.7, 8.0, 11.3, 16.0, 22.6 arcsec) in diameter: 2**(0.5*i)
-root.measurement.algorithms['base_CircularApertureFlux'].radii = [3.0, 4.5, 6.0, 9.0, 12.0, 17.0, 25.0, 35.0, 50.0, 70.0]
+# Activate calibration of measurements: required for aperture corrections
+root.calibrate.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'apertures.py'))
+root.calibrate.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'cmodel.py'))
+root.calibrate.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'kron.py'))
 
+# Activate deep measurements
+root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'apertures.py'))
+root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'kron.py'))
+# Note no CModel: it's slow.
+
+# Enable HSM shapes (unsetup meas_extensions_shapeHSM to disable)
 try:
-    import lsst.meas.extensions.photometryKron
-    root.measurement.algorithms.names |= ["flux.kron"]
+    import lsst.meas.extensions.shapeHSM
+    root.measurement.algorithms.names |= ["shape_hsm_" + alg for alg in
+                                          ("bj", "linear", "ksb", "regauss", "shapelet")]
 except ImportError:
-    print "Cannot import lsst.meas.extensions.photometryKron: disabling Kron measurements"
-
+    print "Cannot import lsst.meas.extensions.shapeHSM: disabling HSM shape measurements"
 
 # Enable deblender for processCcd
 root.measurement.doReplaceWithNoise = True
--- config/processCoadd.py
+++ config/processCoadd.py
@@ -1,27 +1,7 @@
-# Add another aperture flux - it shouldn't be this painful to do (see LSST #2465)
-if False: # This doesn't pickle
-    from lsst.meas.algorithms.algorithmRegistry import AlgorithmRegistry, SincFluxConfig
-    AlgorithmRegistry.register("flux.sinc2", target=SincFluxConfig.Control, ConfigClass=SincFluxConfig)
-    root.measurement.algorithms["flux.sinc2"].radius = 5.0
-    root.measurement.algorithms.names |= ["flux.sinc2"]
-
-root.measurement.algorithms.names |= ["flux.aperture"]
-# Roughly (1.0, 1.4, 2.0, 2.8, 4.0, 5.7, 8.0, 11.3, 16.0, 22.6 arcsec) in diameter: 2**(0.5*i)
-# (assumes 0.168 arcsec pixels on coadd)
-root.measurement.algorithms["flux.aperture"].radii = [3.0, 4.5, 6.0, 9.0, 12.0, 17.0, 25.0, 35.0, 50.0, 70.0]
+import os
+root.calibrate.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'apertures.py'))
+root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'apertures.py'))
+root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'kron.py'))
+root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'cmodel.py'))
 
 root.measurement.slots.instFlux = None
-
-try:
-    import lsst.meas.extensions.photometryKron
-    root.measurement.algorithms.names |= ["flux.kron"]
-except ImportError:
-    print "Unable to import lsst.meas.extensions.photometryKron: Kron fluxes disabled"
-
-try:
-    import lsst.meas.extensions.multiShapelet
-    root.measurement.algorithms.names |= lsst.meas.extensions.multiShapelet.algorithms
-    root.measurement.slots.modelFlux = "multishapelet.combo.flux"
-except ImportError:
-    print "meas_extensions_multiShapelet is not setup; disabling model mags"
-
