7f7db88673061266d9a8ce09b398fcbdf1df7f8d
Paul Price <price@astro.princeton.edu>
2014-09-22 17:32:27 -0400

    Cherry-pick to update genDefectRegistry.py
    genDefectRegistry: support multiple versions of same CCD defects
    
    Multiple versions of the same CCD defects were thrown in the
    database with the same end date (way in the future), so that
    the query to find the defect ended up with multiple rows.
    Instead, have one defect valid until the next, and only the
    last is valid into the future.
    
    Conflicts:
    	bin/genDefectRegistry.py

--- bin/genDefectRegistry.py
+++ bin/genDefectRegistry.py
@@ -9,6 +9,14 @@ except ImportError:
     import sqlite
 import sys
 import argparse
+import datetime
+
+from lsst.pipe.base import Struct
+
+class Row(Struct):
+    def __init__(self, path, version, validStart, validEnd=None):
+        super(Row, self).__init__(path=path, version=version, validStart=validStart, validEnd=validEnd)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--create", action="store_true", help="Create new registry (clobber old)?")
@@ -33,6 +41,7 @@ if makeTables:
 
 cmd = "INSERT INTO defect VALUES (NULL, ?, ?, ?, ?, ?)"
 
+rowsPerCcd = {}
 for f in glob.glob(os.path.join(args.root, "*", "defects*.fits")):
     m = re.search(r'(\S+)/defects_(\d+)\.fits', f)
     if not m:
@@ -43,11 +52,27 @@ for f in glob.glob(os.path.join(args.root, "*", "defects*.fits")):
     #
     f = os.path.relpath(f, args.root)
 
-    if args.verbose:
-        print "Registering %s" % f
-    # New defects should not affect old data
-    startDate = m.group(1).split('/')[1]
-    conn.execute(cmd, (f, m.group(1), int(m.group(2)), startDate, "2037-12-31"))
+    startDate = m.group(1).split('/')[-1]
+    version = m.group(1)
+    ccdSerial = m.group(2)
+    if not ccdSerial in rowsPerCcd:
+        rowsPerCcd[ccdSerial] = []
+    rowsPerCcd[ccdSerial].append(Row(f, version, startDate))
+
+# Fix up end dates so there are no collisions.
+# Defects files for a CCD are valid from the date they are registered until the next date.
+# This means that any defects file should carry ALL the defects that are present at that time.
+for ccd, rowList in rowsPerCcd.iteritems():
+    rowList.sort(key=lambda row: row.validStart) # ISO-8601 will sort just fine without conversion from str
+    for thisRow, nextRow in zip(rowList[:-1], rowList[1:]):
+        thisRow.validEnd = (datetime.datetime.strptime(nextRow.validStart, "%Y-%m-%d") -
+                            datetime.timedelta(0, 1)).isoformat() # 1 sec before: sqlite precision is 1 sec
+    rowList[-1].validEnd = "2037-12-31" # End of UNIX time
+
+    for row in rowList:
+        if args.verbose:
+            print "Registering %s" % row.path
+        conn.execute(cmd, (row.path, row.version, ccd, row.validStart, row.validEnd))
+
 conn.commit()
 conn.close()
-
