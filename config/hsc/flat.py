c37e46a46b81eda7b7b98094f89a36dbe661839a
Naoki Yasuda <yasudank@gmail.com>
2014-11-20 16:09:14 +0900

    Fixed obvious errors caused by introducing VignetteConfig.

--- config/hsc/flat.py
+++ config/hsc/flat.py
@@ -6,7 +6,7 @@ config.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "isr.py")
 
 from lsst.obs.hsc.calibs import HscFlatCombineTask
 config.combination.retarget(HscFlatCombineTask)
-config.combination.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'hsc', 'vignette.py'))
+config.combination.vignette.load(os.path.join(getPackageDir("obs_subaru"), 'config', 'hsc', 'vignette.py'))
 
 config.isr.doBrighterFatter = False
 
