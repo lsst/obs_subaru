faf14f39840d7d8433ec99c97704457cbfbfcb68
Paul Price <price@astro.princeton.edu>
2015-01-21 15:02:09 -0500

    Updates to allow package to build with LSST stack

--- src/distest2.cc
+++ src/distest2.cc
@@ -12,102 +12,6 @@ using namespace std;
 
 namespace {
 
-std::vector< double > InvPROJECTION(std::vector< double >CRVAL,std::vector< double >POS);
-std::vector< double >    PROJECTION(std::vector< double >CRVAL,std::vector< double >POS);
-std::vector< double > CALC_IMPIX(std::vector< double >POS);
-std::vector< double > CALC_IMWLD(std::vector< double >POS);
-std::vector< double > CALC_IMPIX_SIM(std::vector< double >POS);
-std::vector< double > CALC_IMWLD_SIM(std::vector< double >POS);
-
-} // anonymous namespace
-
-namespace lsst {
-namespace obs {
-namespace hsc {
-
-std::vector< double > MAKE_Vdouble(){
-	std::vector< double > A(2);
-	return A;
-}
-std::vector< std::vector< double > > MAKE_VVdouble(){
-	std::vector< std::vector< double > > A;
-	return A;
-}
-
-std::vector< std::vector< double >  > CALC_RADEC(std::vector< double > CRVAL,std::vector< double > CRPIX,std::vector< std::vector< double >  > POSITION){
-	std::vector< std::vector< double >  > DIST_RADEC;
-	std::vector< double > RADEC(2);
-	std::vector< double > POS(2);
-	std::vector< double > POS2(2);
-	int i;
-	for(i=0;i<POSITION.size();i++){
-		POS[0]=POSITION[i][0]-CRPIX[0];
-		POS[1]=POSITION[i][1]-CRPIX[1];
-		POS2=CALC_IMWLD(POS);
-//		RADEC=InvPROJECTION(CRVAL,POS2);
-		RADEC[0]=POS2[0];
-		RADEC[1]=POS2[1];
-		DIST_RADEC.push_back(RADEC);
-	}
-
-	return DIST_RADEC;
-}
-std::vector< std::vector< double >  > CALC_GLOBL(std::vector< double > CRVAL,std::vector< double > CRPIX,std::vector< std::vector< double >  > POSITION){
-	std::vector< std::vector< double >  > DIST_GLOBL;
-	std::vector< double > GLOBL(2);
-	std::vector< double > POS(2);
-	std::vector< double > POS2(2);
-	int i;
-	for(i=0;i<POSITION.size();i++){
-//		POS=PROJECTION(CRVAL,POSITION[i]);
-		POS[0]=POSITION[i][0];
-		POS[1]=POSITION[i][1];
-		POS2=CALC_IMPIX(POS);
-		GLOBL[0]=POS2[0]+CRPIX[0];
-		GLOBL[1]=POS2[1]+CRPIX[1];
-		DIST_GLOBL.push_back(GLOBL);
-	}
-
-	return DIST_GLOBL;
-}
-std::vector< std::vector< double >  > CALC_RADEC_SIM(std::vector< double > CRVAL,std::vector< double > CRPIX,std::vector< std::vector< double >  > POSITION){
-	std::vector< std::vector< double >  > DIST_RADEC;
-	std::vector< double > RADEC(2);
-	std::vector< double > POS(2);
-	std::vector< double > POS2(2);
-	int i;
-	for(i=0;i<POSITION.size();i++){
-		POS[0]=POSITION[i][0]-CRPIX[0];
-		POS[1]=POSITION[i][1]-CRPIX[1];
-		POS2=CALC_IMWLD_SIM(POS);
-//		RADEC=InvPROJECTION(CRVAL,POS2);
-		RADEC[0]=POS2[0];
-		RADEC[1]=POS2[1];
-		DIST_RADEC.push_back(RADEC);
-	}
-
-	return DIST_RADEC;
-}
-std::vector< std::vector< double >  > CALC_GLOBL_SIM(std::vector< double > CRVAL,std::vector< double > CRPIX,std::vector< std::vector< double >  > POSITION){
-	std::vector< std::vector< double >  > DIST_GLOBL;
-	std::vector< double > GLOBL(2);
-	std::vector< double > POS(2);
-	std::vector< double > POS2(2);
-	int i;
-	for(i=0;i<POSITION.size();i++){
-//		POS=PROJECTION(CRVAL,POSITION[i]);
-		POS[0]=POSITION[i][0];
-		POS[1]=POSITION[i][1];
-		POS2=CALC_IMPIX_SIM(POS);
-		GLOBL[0]=POS2[0]+CRPIX[0];
-		GLOBL[1]=POS2[1]+CRPIX[1];
-		DIST_GLOBL.push_back(GLOBL);
-	}
-
-	return DIST_GLOBL;
-}
-//------
-
 #define PI (4*atan(1.0))
 #define LP 180
 std::vector< double > InvPROJECTION(std::vector< double >PPOINT,std::vector< double >Pdeg){
@@ -140,9 +44,10 @@ std::vector< double > PROJECTION(std::vector< double >PPOINT,std::vector< double
 }
 #undef LP
 #undef PI
+
 std::vector< double > CALC_IMPIX(std::vector< double >POS){
 	std::vector< double > POS2(2);
-/*	double COEF[2][10][10]={0};
+/*	double COEF[2][10][10];
 COEF[0][ 0 ][ 0 ]= 0.00357457263523;
 COEF[0][ 0 ][ 1 ]= -21330.9528185;
 COEF[0][ 0 ][ 2 ]= 3.08718040412;
@@ -262,7 +167,7 @@ COEF[1][ 9 ][ 0 ]= -7.26100172315;
 		POS2[1]+=COEF[1][i][j]*pow(POS[0],i)*pow(POS[1],j);
 	}
 */
-	double COEF[4][10*10]={0};
+	double COEF[4][10*10];
 COEF[0][ 0 ]= -0.00047203560468;
 COEF[0][ 1 ]= -1.5427988883e-05;
 COEF[0][ 2 ]= -5.95865625284e-10;
@@ -497,7 +402,7 @@ COEF[3][ 54 ]= 8.09745928121e-38;
 }
 std::vector< double > CALC_IMWLD(std::vector< double >POS){
 	std::vector< double > POS2(2);
-/*	double COEF[2][10][10]={0};
+/*	double COEF[2][10][10];
 COEF[0][ 0 ][ 0 ]= 1.19334688956e-09;
 COEF[0][ 0 ][ 1 ]= -4.68829398468e-05;
 COEF[0][ 0 ][ 2 ]= -6.60313272549e-14;
@@ -617,7 +522,7 @@ COEF[1][ 9 ][ 0 ]= 4.02751121909e-42;
 		POS2[1]+=COEF[1][i][j]*pow(POS[0],i)*pow(POS[1],j);
 	}
 */
-	double COEF[4][10*10]={0};
+	double COEF[4][10*10];
 COEF[0][ 0 ]= -0.00047203560468;
 COEF[0][ 1 ]= -1.5427988883e-05;
 COEF[0][ 2 ]= -5.95865625284e-10;
@@ -850,274 +755,56 @@ COEF[3][ 54 ]= 8.09745928121e-38;
 	}
 	return POS2;
 }
-std::vector< double > CALC_IMPIX_SIM(std::vector< double >POS){
-	std::vector< double > POS1(2);
-	std::vector< double > POS2(2);
-	double Coef[2][10*10]={0},CD[2][2];
 
-	CD[0][0]= 1.0;
-	CD[0][1]= 0.0;
-	CD[1][0]= 0.0;
-	CD[1][1]= 1.0;
-	POS1[0]=( CD[1][1]*POS[0]-CD[0][1]*POS[1])/(CD[0][0]*CD[1][1]-CD[0][1]*CD[1][0]);
-	POS1[1]=(-CD[1][0]*POS[0]+CD[0][0]*POS[1])/(CD[0][0]*CD[1][1]-CD[0][1]*CD[1][0]);
+} // anonymous namespace
 
-    Coef[0][0] = 4.542690e-06;
-    Coef[0][1] = 1.296800e-10;
-    Coef[0][2] = 2.211780e-13;
-    Coef[0][3] = -6.022910e-18;
-    Coef[0][4] = -4.738940e-22;
-    Coef[0][5] = 5.167470e-26;
-    Coef[0][6] = -1.416920e-30;
-    Coef[0][7] = -1.796020e-34;
-    Coef[0][8] = 2.495480e-39;
-    Coef[0][9] = 3.404300e-43;
-    Coef[0][10] = 1.000000e+00;
-    Coef[0][11] = -1.992900e-10;
-    Coef[0][12] = 1.054550e-10;
-    Coef[0][13] = -7.053790e-19;
-    Coef[0][14] = 4.113600e-20;
-    Coef[0][15] = -1.998890e-27;
-    Coef[0][16] = 1.254120e-28;
-    Coef[0][17] = 6.411820e-37;
-    Coef[0][18] = 3.408310e-38;
-    Coef[0][19] = -9.447260e-13;
-    Coef[0][20] = 3.963980e-18;
-    Coef[0][21] = -2.987850e-20;
-    Coef[0][22] = 4.327510e-25;
-    Coef[0][23] = 7.795610e-29;
-    Coef[0][24] = -2.726600e-33;
-    Coef[0][25] = 1.879840e-38;
-    Coef[0][26] = 1.131500e-43;
-    Coef[0][27] = 1.053850e-10;
-    Coef[0][28] = -8.569080e-19;
-    Coef[0][29] = 8.227350e-20;
-    Coef[0][30] = -1.948830e-27;
-    Coef[0][31] = 3.775440e-28;
-    Coef[0][32] = -2.887790e-36;
-    Coef[0][33] = 1.316990e-37;
-    Coef[0][34] = 2.612040e-20;
-    Coef[0][35] = -6.318480e-25;
-    Coef[0][36] = 4.778220e-28;
-    Coef[0][37] = -3.111430e-33;
-    Coef[0][38] = -9.045120e-37;
-    Coef[0][39] = 3.206950e-41;
-    Coef[0][40] = 4.230190e-20;
-    Coef[0][41] = -9.930950e-28;
-    Coef[0][42] = 3.762150e-28;
-    Coef[0][43] = -5.858310e-36;
-    Coef[0][44] = 1.981810e-37;
-    Coef[0][45] = -2.009800e-28;
-    Coef[0][46] = 8.325950e-33;
-    Coef[0][47] = -1.609580e-36;
-    Coef[0][48] = -1.482370e-41;
-    Coef[0][49] = 1.186840e-28;
-    Coef[0][50] = -8.108250e-37;
-    Coef[0][51] = 1.362140e-37;
-    Coef[0][52] = 4.180720e-37;
-    Coef[0][53] = -2.454560e-41;
-    Coef[0][54] = 4.666950e-38;
-    Coef[1][0] = 8.899000e-05;
-    Coef[1][1] = 1.000000e+00;
-    Coef[1][2] = 2.027010e-10;
-    Coef[1][3] = 1.053860e-10;
-    Coef[1][4] = -4.067880e-19;
-    Coef[1][5] = 4.228600e-20;
-    Coef[1][6] = -3.754940e-27;
-    Coef[1][7] = 1.187960e-28;
-    Coef[1][8] = 4.797460e-36;
-    Coef[1][9] = 4.642720e-38;
-    Coef[1][10] = 2.334020e-10;
-    Coef[1][11] = -2.574770e-14;
-    Coef[1][12] = -9.744220e-18;
-    Coef[1][13] = -1.508580e-23;
-    Coef[1][14] = 4.971630e-26;
-    Coef[1][15] = 8.632380e-31;
-    Coef[1][16] = -7.487640e-35;
-    Coef[1][17] = 3.833490e-41;
-    Coef[1][18] = 2.232670e-44;
-    Coef[1][19] = 4.171750e-10;
-    Coef[1][20] = 1.054540e-10;
-    Coef[1][21] = -6.046790e-19;
-    Coef[1][22] = 8.227860e-20;
-    Coef[1][23] = -3.126960e-27;
-    Coef[1][24] = 3.762750e-28;
-    Coef[1][25] = 1.171560e-38;
-    Coef[1][26] = 1.359200e-37;
-    Coef[1][27] = -1.179380e-17;
-    Coef[1][28] = 1.184970e-21;
-    Coef[1][29] = 4.390170e-25;
-    Coef[1][30] = -3.313780e-30;
-    Coef[1][31] = -1.682110e-33;
-    Coef[1][32] = -1.898110e-38;
-    Coef[1][33] = 1.530080e-42;
-    Coef[1][34] = 6.011380e-20;
-    Coef[1][35] = 4.115600e-20;
-    Coef[1][36] = -2.309640e-27;
-    Coef[1][37] = 3.774010e-28;
-    Coef[1][38] = 1.524900e-36;
-    Coef[1][39] = 1.981170e-37;
-    Coef[1][40] = 1.522440e-25;
-    Coef[1][41] = -1.039640e-29;
-    Coef[1][42] = -4.454080e-33;
-    Coef[1][43] = 4.759690e-38;
-    Coef[1][44] = 9.262920e-42;
-    Coef[1][45] = -1.276740e-28;
-    Coef[1][46] = 1.253000e-28;
-    Coef[1][47] = 3.366590e-36;
-    Coef[1][48] = 1.322750e-37;
-    Coef[1][49] = -6.981670e-34;
-    Coef[1][50] = 1.544400e-38;
-    Coef[1][51] = 1.205360e-41;
-    Coef[1][52] = 6.493160e-37;
-    Coef[1][53] = 3.425900e-38;
-    Coef[1][54] = 1.033080e-42;
-	int i,j,ij;
-	ij=0;
-	POS2[0]=POS2[1]=0;
-	for(i=0;i<10;i++)
-	for(j=0;j<10;j++)
-	if( i + j<10){
-		POS2[0]+=Coef[0][ij]*pow(POS1[0],i)*pow(POS1[1],j);
-		POS2[1]+=Coef[1][ij]*pow(POS1[0],i)*pow(POS1[1],j);
-		ij++;
-	}
-	return POS2;
+
+namespace lsst {
+namespace obs {
+namespace hsc {
+
+std::vector< double > MAKE_Vdouble(){
+	std::vector< double > A(2);
+	return A;
 }
-std::vector< double > CALC_IMWLD_SIM(std::vector< double >POS){
-	std::vector< double > POS1(2);
-	std::vector< double > POS2(2);
-	double Coef[2][10*10]={0},CD[2][2];
-
-	CD[0][0]= 1.0;
-	CD[0][1]= 0.0;
-	CD[1][0]= 0.0;
-	CD[1][1]= 1.0;
-
-    Coef[0][0] = -4.380370e-06;
-    Coef[0][1] = -1.995040e-10;
-    Coef[0][2] = -1.817160e-13;
-    Coef[0][3] = 5.617380e-18;
-    Coef[0][4] = 2.964630e-22;
-    Coef[0][5] = -4.136840e-26;
-    Coef[0][6] = 1.251490e-30;
-    Coef[0][7] = 1.350210e-34;
-    Coef[0][8] = -1.833610e-39;
-    Coef[0][9] = -2.459310e-43;
-    Coef[0][10] = 1.000000e+00;
-    Coef[0][11] = 2.007060e-10;
-    Coef[0][12] = -1.055900e-10;
-    Coef[0][13] = 6.600740e-19;
-    Coef[0][14] = -6.189660e-21;
-    Coef[0][15] = 1.620700e-27;
-    Coef[0][16] = -1.122810e-28;
-    Coef[0][17] = -2.491050e-36;
-    Coef[0][18] = 1.018330e-37;
-    Coef[0][19] = 8.616620e-13;
-    Coef[0][20] = 1.040290e-17;
-    Coef[0][21] = 2.342430e-20;
-    Coef[0][22] = -4.253810e-25;
-    Coef[0][23] = -5.310430e-29;
-    Coef[0][24] = 2.046700e-33;
-    Coef[0][25] = -2.211180e-38;
-    Coef[0][26] = 1.731020e-43;
-    Coef[0][27] = -1.055360e-10;
-    Coef[0][28] = 7.971890e-19;
-    Coef[0][29] = -1.237090e-20;
-    Coef[0][30] = 1.561240e-27;
-    Coef[0][31] = -3.377410e-28;
-    Coef[0][32] = -3.722440e-36;
-    Coef[0][33] = 4.102940e-37;
-    Coef[0][34] = -2.256700e-20;
-    Coef[0][35] = 1.831590e-25;
-    Coef[0][36] = -3.534470e-28;
-    Coef[0][37] = 3.929320e-33;
-    Coef[0][38] = 6.098000e-37;
-    Coef[0][39] = -2.359170e-41;
-    Coef[0][40] = -7.040310e-21;
-    Coef[0][41] = 7.398160e-28;
-    Coef[0][42] = -3.369280e-28;
-    Coef[0][43] = -1.452360e-36;
-    Coef[0][44] = 6.150350e-37;
-    Coef[0][45] = 1.645070e-28;
-    Coef[0][46] = -4.046500e-33;
-    Coef[0][47] = 1.108910e-36;
-    Coef[0][48] = 4.139220e-42;
-    Coef[0][49] = -1.076870e-28;
-    Coef[0][50] = -1.198030e-36;
-    Coef[0][51] = 4.076940e-37;
-    Coef[0][52] = -3.242150e-37;
-    Coef[0][53] = 1.283090e-41;
-    Coef[0][54] = 9.379990e-38;
-    Coef[1][0] = -8.286180e-05;
-    Coef[1][1] = 1.000000e+00;
-    Coef[1][2] = -2.043080e-10;
-    Coef[1][3] = -1.055360e-10;
-    Coef[1][4] = 5.734950e-19;
-    Coef[1][5] = -7.031790e-21;
-    Coef[1][6] = 2.754290e-27;
-    Coef[1][7] = -1.077510e-28;
-    Coef[1][8] = -4.929120e-36;
-    Coef[1][9] = 9.393580e-38;
-    Coef[1][10] = -2.012200e-10;
-    Coef[1][11] = 5.215440e-14;
-    Coef[1][12] = 8.202230e-18;
-    Coef[1][13] = -1.367250e-22;
-    Coef[1][14] = -3.571670e-26;
-    Coef[1][15] = -6.255230e-31;
-    Coef[1][16] = 4.801170e-35;
-    Coef[1][17] = -8.679450e-41;
-    Coef[1][18] = -1.184220e-44;
-    Coef[1][19] = -4.173330e-10;
-    Coef[1][20] = -1.055890e-10;
-    Coef[1][21] = 8.467120e-19;
-    Coef[1][22] = -1.238130e-20;
-    Coef[1][23] = 2.317620e-27;
-    Coef[1][24] = -3.369240e-28;
-    Coef[1][25] = -4.791800e-36;
-    Coef[1][26] = 4.077870e-37;
-    Coef[1][27] = 9.621110e-18;
-    Coef[1][28] = -2.268360e-21;
-    Coef[1][29] = -3.570260e-25;
-    Coef[1][30] = 7.894370e-30;
-    Coef[1][31] = 1.145440e-33;
-    Coef[1][32] = 1.340330e-38;
-    Coef[1][33] = -9.379170e-43;
-    Coef[1][34] = 7.940900e-20;
-    Coef[1][35] = -6.207820e-21;
-    Coef[1][36] = 1.734180e-27;
-    Coef[1][37] = -3.375970e-28;
-    Coef[1][38] = -5.680250e-36;
-    Coef[1][39] = 6.149470e-37;
-    Coef[1][40] = -1.170020e-25;
-    Coef[1][41] = 2.081400e-29;
-    Coef[1][42] = 3.516930e-33;
-    Coef[1][43] = -6.731210e-38;
-    Coef[1][44] = -5.956520e-42;
-    Coef[1][45] = 7.247450e-29;
-    Coef[1][46] = -1.121890e-28;
-    Coef[1][47] = -3.616860e-36;
-    Coef[1][48] = 4.098260e-37;
-    Coef[1][49] = 5.022440e-34;
-    Coef[1][50] = -4.451430e-38;
-    Coef[1][51] = -9.308030e-42;
-    Coef[1][52] = -2.769340e-37;
-    Coef[1][53] = 1.017020e-37;
-    Coef[1][54] = -6.874890e-43;
-	int i,j,ij;
-	ij=0;
-	POS1[0]=POS1[1]=0;
-	for(i=0;i<10;i++)
-	for(j=0;j<10;j++)
-	if( i + j<10){
-		POS1[0]+=Coef[0][ij]*pow(POS[0],i)*pow(POS[1],j);
-		POS1[1]+=Coef[1][ij]*pow(POS[0],i)*pow(POS[1],j);
-		ij++;
-	}
-	POS2[0]=CD[0][0]*POS1[0]+CD[0][1]*POS1[1];
-	POS2[1]=CD[1][0]*POS1[0]+CD[1][1]*POS1[1];
-	return POS2;
+std::vector< std::vector< double > > MAKE_VVdouble(){
+	std::vector< std::vector< double > > A;
+	return A;
 }
 
+std::vector< std::vector< double >  > CALC_RADEC(std::vector< double > CRVAL,std::vector< double > CRPIX,std::vector< std::vector< double >  > POSITION){
+	std::vector< std::vector< double >  > DIST_RADEC;
+	std::vector< double > RADEC(2);
+	std::vector< double > POS(2);
+	std::vector< double > POS2(2);
+	for(size_t i=0;i<POSITION.size();i++){
+		POS[0]=POSITION[i][0]-CRPIX[0];
+		POS[1]=POSITION[i][1]-CRPIX[1];
+		POS2=CALC_IMWLD(POS);
+//		RADEC=InvPROJECTION(CRVAL,POS2);
+		RADEC[0]=POS2[0];
+		RADEC[1]=POS2[1];
+		DIST_RADEC.push_back(RADEC);
+	}
+
+	return DIST_RADEC;
+}
+std::vector< std::vector< double >  > CALC_GLOBL(std::vector< double > CRVAL,std::vector< double > CRPIX,std::vector< std::vector< double >  > POSITION){
+	std::vector< std::vector< double >  > DIST_GLOBL;
+	std::vector< double > GLOBL(2);
+	std::vector< double > POS(2);
+	std::vector< double > POS2(2);
+	for(size_t i=0;i<POSITION.size();i++){
+//		POS=PROJECTION(CRVAL,POSITION[i]);
+		POS[0]=POSITION[i][0];
+		POS[1]=POSITION[i][1];
+		POS2=CALC_IMPIX(POS);
+		GLOBL[0]=POS2[0]+CRPIX[0];
+		GLOBL[1]=POS2[1]+CRPIX[1];
+		DIST_GLOBL.push_back(GLOBL);
+	}
+
+	return DIST_GLOBL;
+}
 
 }}} // namespace lsst::obs::hsc
