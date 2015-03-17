927f8960f413190126a7e314d634bfcfe0ef935f
Paul Price <price@astro.princeton.edu>
2015-03-17 10:25:34 -0400

    config: set maxNumberOfPeaks for deblending on coadds
    In both processCoadd and the multiband processing scheme.
    
    Conflicts:
    	config/processCoadd.py

--- config/measureCoaddSources.py
+++ config/measureCoaddSources.py
@@ -2,3 +2,5 @@ import os
 root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'apertures.py'))
 root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'kron.py'))
 root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'cmodel.py'))
+
+root.deblend.maxNumberOfPeaks = 20
--- config/processCoadd.py
+++ config/processCoadd.py
@@ -5,3 +5,12 @@ root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'kron
 root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'cmodel.py'))
 
 root.measurement.slots.instFlux = None
+
+# Enable HSM shapes (unsetup meas_extensions_shapeHSM to disable)
+try:
+    root.measurement.load(os.path.join(os.environ['MEAS_EXTENSIONS_SHAPEHSM_DIR'], 'config', 'enable.py'))
+    root.measurement.algorithms["shape.hsm.regauss"].deblendNChild = "deblend.nchild"
+except:
+    print "Cannot enable shapeHSM: disabling shear measurement"
+
+root.deblend.maxNumberOfPeaks = 20
