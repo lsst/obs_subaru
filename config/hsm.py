a8faf26393f63d721d5f00025a16ec67f1f537d7
Paul Price <price@astro.princeton.edu>
2015-03-25 14:08:50 -0400

    config: set HSM moments as default shape
    We believe HSM is more robust than the SDSS adaptive moments.
    It had not been set as default earlier due to an oversight.

--- config/hsm.py
+++ config/hsm.py
@@ -4,5 +4,6 @@ import os
 try:
     root.load(os.path.join(os.environ['MEAS_EXTENSIONS_SHAPEHSM_DIR'], 'config', 'enable.py'))
     root.algorithms["shape.hsm.regauss"].deblendNChild = "deblend.nchild"
+    root.slots.shape = "shape.hsm.moments"
 except Exception as e:
     print "Cannot enable shapeHSM (%s): disabling HSM shape measurements" % (e,)
