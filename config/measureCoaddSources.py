382fce35c56df5f906b524b44eea53d846617b01
Paul Price <price@astro.princeton.edu>
2015-03-25 13:57:16 -0400

    config: activate HSM for measureCoaddSources

--- config/measureCoaddSources.py
+++ config/measureCoaddSources.py
@@ -2,5 +2,6 @@ import os
 root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'apertures.py'))
 root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'kron.py'))
 root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'cmodel.py'))
+root.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'hsm.py'))
 
 root.deblend.maxNumberOfPeaks = 20
