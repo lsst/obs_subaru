2b98b9ddd01870109ace8b0b1b90d680c1458f3a
Paul Price <price@astro.princeton.edu>
2015-04-02 18:17:33 +1000

    isr: skip rough zero point for non-positive exposure time
    A non-positive exposure time (e.g., for bias frames) would cause a
    math domain error, so guard against that case.

--- python/lsst/obs/subaru/isr.py
+++ python/lsst/obs/subaru/isr.py
@@ -578,6 +578,9 @@ class SubaruIsrTask(IsrTask):
             self.log.warn("No rough magnitude zero point set for filter %s" % filterName)
             fluxMag0 = self.config.defaultFluxMag0T1
         expTime = exposure.getCalib().getExptime()
+        if expTime <= 0:
+            self.log.warn("Non-positive exposure time; skipping rough zero point")
+            return
         self.log.info("Setting rough magnitude zero point: %f" % (2.5*math.log10(fluxMag0*expTime),))
         exposure.getCalib().setFluxMag0(fluxMag0*expTime)
 
