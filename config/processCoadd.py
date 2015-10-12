5e8882c901981529f5dba18caf3ae5e29416b81d
Lauren MacArthur <lauren@astro.princeton.edu>
2015-10-12 14:51:24 -0400

    Update processCoadd config to bypass cmodel
    The cmodel fitting code is undergoing a significant overhaul.  Given
    the large runtime and occasional failures, we here turn off cmodel
    while we await these latest fixes (large blends, footprint merging,
    etc.) are in.
    
    This is an update to the following commit where cmodel was turned
    off in the other config override files:
    https://github.com/lsst/obs_subaru/commit/2874722faf7c2a601f5f286c868ecd810127befb

--- config/processCoadd.py
+++ config/processCoadd.py
@@ -1,7 +1,8 @@
 import os
 config.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'apertures.py'))
 config.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'kron.py'))
-config.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'cmodel.py'))
+# Turn off cmodel until latest fixes (large blends, footprint merging, etc.) are in
+# config.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'cmodel.py'))
 config.measurement.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'hsm.py'))
 
 config.measurement.slots.instFlux = None
