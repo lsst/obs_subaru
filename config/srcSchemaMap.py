cf7fa355aa52bb728c6a58e1b1b570fa5f23f1ab
Lauren MacArthur <lauren@astro.princeton.edu>
2015-12-29 18:53:23 -0500

    Add a mapper between the HSC/LSST src schemas

---
+++ config/srcSchemaMap.py
@@ -0,0 +1,209 @@
+config.srcSchemaMap = {
+"deblend_nChild": "deblend_nchild",
+"deblend_deblendedAsPsf": "deblend_deblended-as-psf",
+"deblend_psfCenter_x": "deblend_psf-center_x",
+"deblend_psfCenter_y": "deblend_psf-center_y",
+"deblend_psfFlux": "deblend_psf-flux",
+"deblend_tooManyPeaks": "deblend_too-many-peaks",
+"deblend_parentTooBig": "deblend_parent-too-big",
+"deblend_rampedTemplate": "deblend_ramped_template",
+"deblend_patchedTemplate": "deblend_patched_template",
+"deblend_hasStrayFlux": "deblend_has_stray_flux",
+"base_GaussianCentroid_x": "no_equivalent",
+"base_GaussianCentroid_y": "no_equivalent",
+"base_GaussianCentroid_flag": "no_equivalent",
+"base_GaussianCentroid_flag_noPeak": "no_equivalent",
+"base_NaiveCentroid_x": "centroid_naive_x",
+"base_NaiveCentroid_y": "centroid_naive_y",
+"base_NaiveCentroid_flag": "centroid_naive_flag",
+"base_NaiveCentroid_flag_noCounts": "no_equivalent",
+"base_NaiveCentroid_flag_edge": "no_equivalent",
+"base_SdssCentroid_x": "centroid_sdss_x",
+"base_SdssCentroid_y": "centroid_sdss_y",
+"base_SdssCentroid_xSigma": "centroid_sdss_err_xSigma",
+"base_SdssCentroid_ySigma": "centroid_sdss_err_ySigma",
+"base_SdssCentroid_flag": "centroid_sdss_flag",
+"base_SdssCentroid_flag_edge": "no_equivalent",
+"base_SdssCentroid_flag_noSecondDerivative": "no_equivalent",
+"base_SdssCentroid_flag_almostNoSecondDerivative": "no_equivalent",
+"base_SdssCentroid_flag_notAtMaximum": "no_equivalent",
+"base_SdssShape_xx": "shape_sdss_xx",
+"base_SdssShape_yy": "shape_sdss_yy",
+"base_SdssShape_xy": "shape_sdss_xy",
+"base_SdssShape_xxSigma": "shape_sdss_err_xxSigma",
+"base_SdssShape_yySigma": "shape_sdss_err_yySigma",
+"base_SdssShape_xySigma": "shape_sdss_err_xySigma",
+"base_SdssShape_x": "shape_sdss_centroid_x",
+"base_SdssShape_y": "shape_sdss_centroid_y",
+"base_SdssShape_flux": "no_equivalent",
+"base_SdssShape_fluxSigma": "no_equivalent",
+"base_SdssShape_flux_xx_Cov": "no_equivalent",
+"base_SdssShape_flux_yy_Cov": "no_equivalent",
+"base_SdssShape_flux_xy_Cov": "no_equivalent",
+"base_SdssShape_flag": "shape_sdss_flag",
+"base_SdssShape_flag_unweightedBad": "shape_sdss_flag_unweightedbad",
+"base_SdssShape_flag_unweighted": "shape_sdss_flag_unweighted",
+"base_SdssShape_flag_shift": "shape_sdss_flag_shift",
+"base_SdssShape_flag_maxIter": "shape_sdss_flag_maxiter",
+"base_CircularApertureFlux_3_0_flux": "no_equivalent",
+"base_CircularApertureFlux_3_0_fluxSigma": "no_equivalent",
+"base_CircularApertureFlux_3_0_flag": "no_equivalent_flag",
+"base_CircularApertureFlux_3_0_flag_apertureTruncated": "no_equivalent",
+"base_CircularApertureFlux_3_0_flag_sincCoeffsTruncated": "no_equivalent",
+"base_CircularApertureFlux_4_5_flux": "no_equivalent",
+"base_CircularApertureFlux_4_5_fluxSigma": "no_equivalent",
+"base_CircularApertureFlux_4_5_flag": "no_equivalent_flag",
+"base_CircularApertureFlux_4_5_flag_apertureTruncated": "no_equivalent",
+"base_CircularApertureFlux_4_5_flag_sincCoeffsTruncated": "no_equivalent",
+"base_CircularApertureFlux_6_0_flux": "no_equivalent",
+"base_CircularApertureFlux_6_0_fluxSigma": "no_equivalent",
+"base_CircularApertureFlux_6_0_flag": "no_equivalent_flag",
+"base_CircularApertureFlux_6_0_flag_apertureTruncated": "no_equivalent",
+"base_CircularApertureFlux_6_0_flag_sincCoeffsTruncated": "no_equivalent",
+"base_CircularApertureFlux_9_0_flux": "no_equivalent",
+"base_CircularApertureFlux_9_0_fluxSigma": "no_equivalent",
+"base_CircularApertureFlux_9_0_flag": "no_equivalent_flag",
+"base_CircularApertureFlux_9_0_flag_apertureTruncated": "no_equivalent",
+"base_CircularApertureFlux_9_0_flag_sincCoeffsTruncated": "no_equivalent",
+"base_CircularApertureFlux_12_0_flux": "no_equivalent",
+"base_CircularApertureFlux_12_0_fluxSigma": "no_equivalent",
+"base_CircularApertureFlux_12_0_flag": "no_equivalent_flag",
+"base_CircularApertureFlux_12_0_flag_apertureTruncated": "no_equivalent",
+"base_CircularApertureFlux_12_0_flag_sincCoeffsTruncated": "no_equivalent",
+"base_CircularApertureFlux_17_0_flux": "no_equivalent",
+"base_CircularApertureFlux_17_0_fluxSigma": "no_equivalent",
+"base_CircularApertureFlux_17_0_flag": "no_equivalent_flag",
+"base_CircularApertureFlux_17_0_flag_apertureTruncated": "no_equivalent",
+"base_CircularApertureFlux_25_0_flux": "no_equivalent",
+"base_CircularApertureFlux_25_0_fluxSigma": "no_equivalent",
+"base_CircularApertureFlux_25_0_flag": "no_equivalent_flag",
+"base_CircularApertureFlux_25_0_flag_apertureTruncated": "no_equivalent",
+"base_CircularApertureFlux_35_0_flux": "no_equivalent",
+"base_CircularApertureFlux_35_0_fluxSigma": "no_equivalent",
+"base_CircularApertureFlux_35_0_flag": "no_equivalent_flag",
+"base_CircularApertureFlux_35_0_flag_apertureTruncated": "no_equivalent",
+"base_CircularApertureFlux_50_0_flux": "no_equivalent",
+"base_CircularApertureFlux_50_0_fluxSigma": "no_equivalent",
+"base_CircularApertureFlux_50_0_flag": "no_equivalent_flag",
+"base_CircularApertureFlux_50_0_flag_apertureTruncated": "no_equivalent",
+"base_CircularApertureFlux_70_0_flux": "no_equivalent",
+"base_CircularApertureFlux_70_0_fluxSigma": "no_equivalent",
+"base_CircularApertureFlux_70_0_flag": "no_equivalent_flag",
+"base_CircularApertureFlux_70_0_flag_apertureTruncated": "no_equivalent",
+"base_GaussianFlux_flux": "flux_gaussian",
+"base_GaussianFlux_fluxSigma": "flux_gaussian_err",
+"base_GaussianFlux_flag": "flux_gaussian_flag",
+"base_PixelFlags_flag": "no_equivalent",
+"base_PixelFlags_flag_edge": "flag_pixel_edge",
+"base_PixelFlags_flag_offimage": "flag_pixel_offimage",
+"base_PixelFlags_flag_interpolated": "flag_pixel_interpolated_any",
+"base_PixelFlags_flag_interpolatedCenter": "flag_pixel_interpolated_center",
+"base_PixelFlags_flag_saturated": "flag_pixel_saturated_any",
+"base_PixelFlags_flag_saturatedCenter": "flag_pixel_saturated_center",
+"base_PixelFlags_flag_suspect": "flag_pixel_suspect_any",
+"base_PixelFlags_flag_suspectCenter": "flag_pixel_suspect_center",
+"base_PixelFlags_flag_cr": "flag_pixel_cr_any",
+"base_PixelFlags_flag_crCenter": "flag_pixel_cr_center",
+"base_PixelFlags_flag_bad": "flag_pixel_bad",
+"base_PsfFlux_flux": "flux_psf",
+"base_PsfFlux_fluxSigma": "flux_psf_err",
+"base_PsfFlux_flag": "flux_psf_flag",
+"base_PsfFlux_flag_noGoodPixels": "no_equivalent",
+"base_PsfFlux_flag_edge": "no_equivalent",
+"ext_photometryKron_KronFlux_flux": "flux_kron",
+"ext_photometryKron_KronFlux_fluxSigma": "flux_kron_err",
+"ext_photometryKron_KronFlux_radius": "flux_kron_radius",
+"ext_photometryKron_KronFlux_radius_for_radius": "flux_kron_radiusForRadius",
+"ext_photometryKron_KronFlux_psf_radius": "flux_kron_psfRadius",
+"ext_photometryKron_KronFlux_flag": "flux_kron_flag",
+"ext_photometryKron_KronFlux_flag_edge": "flux_kron_flag_edge",
+"ext_photometryKron_KronFlux_flag_bad_shape_no_psf": "no_equivalent",
+"ext_photometryKron_KronFlux_flag_no_minimum_radius": "no_equivalent",
+"ext_photometryKron_KronFlux_flag_no_fallback_radius": "no_equivalent",
+"ext_photometryKron_KronFlux_flag_radius": "flux_kron_flag_radius",
+"ext_photometryKron_KronFlux_flag_used_minimum_radius": "flux_kron_flag_usedMinimumRadius",
+"ext_photometryKron_KronFlux_flag_used_psf_radius": "flux_kron_flag_usedPsfRadius",
+"ext_photometryKron_KronFlux_flag_small_radius": "flux_kron_flag_smallRadius",
+"ext_photometryKron_KronFlux_flag_bad_shape": "flux_kron_flag_badShape",
+"base_FocalPlane_x": "focalplane_x",
+"base_FocalPlane_y": "focalplane_y",
+"base_FocalPlane_flag": "no_equivalent",
+"base_Jacobian_value": "jacobian",
+"base_Jacobian_flag": "no_equivalent",
+"base_ClassificationExtendedness_value": "classification_extendedness",
+"base_ClassificationExtendedness_flag": "no_equivalent",
+"base_PsfFlux_apCorr": "flux_psf_apcorr",
+"base_PsfFlux_apCorrSigma": "flux_psf_apcorr_err",
+"base_PsfFlux_flag_apCorr": "flux_psf_flag_apcorr",
+"ext_photometryKron_KronFlux_apCorr": "flux_kron_apcorr",
+"ext_photometryKron_KronFlux_apCorrSigma": "flux_kron_apcorr_err",
+"ext_photometryKron_KronFlux_flag_apCorr": "flux_kron_flag_apcorr",
+"base_GaussianFlux_apCorr": "flux_gaussian_apcorr",
+"base_GaussianFlux_apCorrSigma": "flux_gaussian_apcorr_err",
+"base_GaussianFlux_flag_apCorr": "flux_gaussian_flag_apcorr",
+"calib_psfUsed": "calib_psf_used",
+"calib_psfCandidate": "calib_psf_candidate",
+"calib_psfReserved": "calib_psf_reserved",
+}
+
+#========== No equivalents below =======
+#"flag_badcentroid"
+#"flux_naive_err"
+#"flux_naive_flag"
+#"flux_sinc"
+#"flux_sinc_err"
+#"flux_sinc_flag"
+#"flux_aperture_nInterpolatedPixel"
+#"flux_aperture_nProfile"
+#"shape_hsm_moments_xx"
+#"shape_hsm_moments_yy"
+#"shape_hsm_moments_xy"
+#"shape_hsm_moments_err_xxSigma"
+#"shape_hsm_moments_err_yySigma"
+#"shape_hsm_moments_err_xySigma"
+#"shape_hsm_moments_err_xx_yy_Cov"
+#"shape_hsm_moments_err_xx_xy_Cov"
+#"shape_hsm_moments_err_yy_xy_Cov"
+#"shape_hsm_moments_flag"
+#"shape_hsm_moments_centroid_x"
+#"shape_hsm_moments_centroid_y"
+#"shape_hsm_moments_centroid_err_xSigma"
+#"shape_hsm_moments_centroid_err_ySigma"
+#"shape_hsm_moments_centroid_err_x_y_Cov"
+#"shape_hsm_moments_centroid_flag"
+#"shape_hsm_psfMoments_xx"
+#"shape_hsm_psfMoments_yy"
+#"shape_hsm_psfMoments_xy"
+#"shape_hsm_psfMoments_err_xxSigma"
+#"shape_hsm_psfMoments_err_yySigma"
+#"shape_hsm_psfMoments_err_xySigma"
+#"shape_hsm_psfMoments_err_xx_yy_Cov"
+#"shape_hsm_psfMoments_err_xx_xy_Cov"
+#"shape_hsm_psfMoments_err_yy_xy_Cov"
+#"shape_hsm_psfMoments_flag"
+#"shape_hsm_psfMoments_centroid_x"
+#"shape_hsm_psfMoments_centroid_y"
+#"shape_hsm_psfMoments_centroid_err_xSigma"
+#"shape_hsm_psfMoments_centroid_err_ySigma"
+#"shape_hsm_psfMoments_centroid_err_x_y_Cov"
+#"shape_hsm_psfMoments_centroid_flag"
+#"shape_hsm_regauss_e1"
+#"shape_hsm_regauss_e2"
+#"shape_hsm_regauss_sigma"
+#"shape_hsm_regauss_resolution"
+#"shape_hsm_regauss_flag"
+#"centroid_naive_err_xSigma"
+#"centroid_naive_err_ySigma"
+#"centroid_sdss_err_x_y_Cov"
+#"centroid_naive_err_x_y_Cov"
+#"shape_sdss_err_xx_yy_Cov"
+#"shape_sdss_err_xx_xy_Cov"
+#"shape_sdss_err_yy_xy_Cov"
+#"shape_sdss_centroid_err_xSigma"
+#"shape_sdss_centroid_err_ySigma"
+#"shape_sdss_centroid_err_x_y_Cov"
+#"shape_sdss_psf_xx"
+#"shape_sdss_psf_yy"
+#"shape_sdss_psf_xy"
+#"shape_sdss_flag_psf"
+#"shape_sdss_centroid_flag"
