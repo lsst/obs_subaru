e8df9cb3a2954cad72ae3078b76588dddd27de98
Russell Owen <rowen@uw.edu>
2016-02-01 13:50:55 -0800

    Update processCcd.py config override files

--- config/hsc/processCcd.py
+++ config/hsc/processCcd.py
@@ -8,10 +8,10 @@ from lsst.utils import getPackageDir
 
 hscConfigDir = os.path.join(getPackageDir("obs_subaru"), "config", "hsc")
 config.load(os.path.join(hscConfigDir, 'isr.py'))
-config.calibrate.photocal.colorterms.load(os.path.join(hscConfigDir, 'colorterms.py'))
-config.calibrate.measurePsf.starSelector.name='objectSize'
-config.calibrate.measurePsf.starSelector['objectSize'].widthMin=0.9
-config.calibrate.measurePsf.starSelector['objectSize'].fluxMin=4000
+config.calibrate.photoCal.colorterms.load(os.path.join(hscConfigDir, 'colorterms.py'))
+config.charImage.measurePsf.starSelector.name='objectSize'
+config.charImage.measurePsf.starSelector['objectSize'].widthMin=0.9
+config.charImage.measurePsf.starSelector['objectSize'].fluxMin=4000
 
 config.calibrate.astrometry.wcsFitter.order = 3
 config.calibrate.astrometry.matcher.maxMatchDistArcSec = 2.0
