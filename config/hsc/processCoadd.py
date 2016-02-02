53defdeb58205ee9ba47de5083dce7c89e6140dc
Russell Owen <rowen@uw.edu>
2016-02-02 15:47:00 -0800

    Remove processCoadd config override files

--- config/hsc/processCoadd.py
+++
@@ -1,8 +0,0 @@
-"""
-HSC-specific overrides for ProcessCoaddTask
-(applied after Subaru overrides in ../processCoadd.py).
-"""
-
-config.detection.background.useApprox = False
-config.detection.background.binSize = 4096
-config.detection.background.undersampleStyle = 'REDUCE_INTERP_ORDER'
--- config/suprimecam-mit/processCoadd.py
+++
@@ -1,6 +0,0 @@
-import os.path
-
-from lsst.utils import getPackageDir
-
-suprimecamMitConfigDir = os.path.join(getPackageDir("obs_subaru"), "config", "suprimecam-mit")
-config.calibrate.photocal.colorterms.load(os.path.join(suprimecamMitConfigDir, 'colorterms.py'))
