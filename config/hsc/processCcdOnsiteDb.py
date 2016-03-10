e34e9a5030eec7b8404a378bdc3bfffd1b5d767f
Paul Price <price@astro.princeton.edu>
2016-03-10 14:13:39 -0500

    config: remove old config files
    These aren't used any more.

--- config/hsc/processCcdOnsiteDb.py
+++
@@ -1,9 +0,0 @@
-from hsc.pipe.tasks.onsiteDb import HscOnsiteDbTask
-config.onsiteDb.retarget(HscOnsiteDbTask)
-
-# Load regular processCcd configuration
-import os.path
-
-from lsst.utils import getPackageDir
-
-config.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "processCcd.py"))
--- config/hsc/processStack.py
+++
@@ -1,6 +0,0 @@
-"""
-HSC-specific overrides for ProcessStackTask
-(applied after Subaru overrides in ../processStack.py).
-"""
-
-config.calibrate.measurePsf.starSelector.name = "secondMoment" # "objectSize" has problems with corner CCDs
