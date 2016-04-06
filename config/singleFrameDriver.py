8a2eee8789575b54782c64d5a5eb699a3d7a93c2
Nate Lust <nlust@astro.princeton.edu>
2016-04-06 12:58:51 -0400

    Update name of driver script to singleFrameDriver

--- config/singleFrameDriver.py
+++ config/singleFrameDriver.py
@@ -1,8 +1,9 @@
 """
-Subaru-specific overrides for ProcessExposureTask (applied before SuprimeCam- and HSC-specific overrides).
+Subaru-specific overrides for SingleFrameDriverTask (applied before SuprimeCam- and HSC-specific overrides).
 """
 import os.path
 
 from lsst.utils import getPackageDir
 
 config.processCcd.load(os.path.join(getPackageDir("obs_subaru"), "config", "processCcd.py"))
+config.ccdKey = 'ccd'
