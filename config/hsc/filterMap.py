510e63560f48879ca20b70ad7712db02c15817bc
Lauren MacArthur <lauren@astro.princeton.edu>
2016-04-15 10:53:19 -0400

    Update filterMap and loading in configs

--- config/hsc/filterMap.py
+++ config/hsc/filterMap.py
@@ -1,9 +1,9 @@
-for source, target in [("N921", 'z'), ("N816", 'i'), ("N515", 'g'), ("N387", 'g'), ("N1010", 'z')]:
-    config.filterMap[source] = target
-
 from lsst.pipe.tasks.setConfigFromEups import setAstrometryConfigFromEups
 menu = { "ps1*": {}, # Defaults are fine
-         "sdss*": {"refObjLoader.filterMap": {"y": "z"}}, # No y-band, use z instead
-         "2mass*": {"refObjLoader.filterMap": {ff: "J" for ff in "grizy"}}, # No optical bands, use J instead
+         "sdss*": {"filterMap": {"y": "z"}}, # No y-band, use z instead
+         "2mass*": {"filterMap": {ff: "J" for ff in "grizy"}}, # No optical bands, use J instead
         }
 setAstrometryConfigFromEups(config, menu)
+
+for source, target in [("N921", 'z'), ("N816", 'i'), ("N515", 'g'), ("N387", 'g'), ("N1010", 'z')]:
+    config.filterMap[source] = target
