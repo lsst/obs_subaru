0318aef4282ee855354a018ebdecaea07043ce15
Colin Slater <ctslater@umich.edu>
2016-05-02 17:42:54 -0500

    Fix broken suprimecam-mit config override.

--- config/suprimecam-mit/processCcd.py
+++ config/suprimecam-mit/processCcd.py
@@ -8,7 +8,7 @@ from lsst.utils import getPackageDir
 
 suprimecamMitConfigDir = os.path.join(getPackageDir("obs_subaru"), "config", "suprimecam-mit")
 config.load(os.path.join(suprimecamMitConfigDir, 'isr.py'))
-config.calibrate.photocal.colorterms.load(os.path.join(suprimecamMitConfigDir, 'colorterms.py'))
+config.calibrate.photoCal.colorterms.load(os.path.join(suprimecamMitConfigDir, 'colorterms.py'))
 
 config.charImage.detectAndMeasure.measurement.plugins["base_Jacobian"].pixelScale = 0.2
 config.calibrate.detectAndMeasure.measurement.plugins["base_Jacobian"].pixelScale = 0.2
