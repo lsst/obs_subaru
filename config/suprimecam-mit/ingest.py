9260ae163e1d4ec622c62055ef9dd7472a1e7207
Colin Slater <ctslater@umich.edu>
2016-05-02 14:15:23 -0500

    Add ingest config entries for suprimecam-mit.
    config.parse.translation['pa'] needs to be 'INR-PA' (instead of 'INST-PA' in
    HSC), and config.parse.defaults needs the additional fields 'autoguider' and
    'config'.

---
+++ config/suprimecam-mit/ingest.py
@@ -0,0 +1,19 @@
+
+config.parse.defaults = {'ccdTemp': '0',
+                         'config': 'configuration',
+                         'autoguider': '0'
+                        }
+
+config.parse.translation = {'proposal': 'PROP-ID',
+                            'dataType': 'DATA-TYP',
+                            'expTime': 'EXPTIME',
+                            'ccd': 'DET-ID',
+                            'pa': 'INR-STR',
+                            'autoguider': 'T_AG',
+                            'ccdTemp': 'T_CCDTV',
+                            'config': 'T_CFGFIL',
+                            'frameId': 'FRAMEID',
+                            'expId': 'EXP-ID',
+                            'dateObs': 'DATE-OBS',
+                            'taiObs': 'DATE-OBS',
+                           }
