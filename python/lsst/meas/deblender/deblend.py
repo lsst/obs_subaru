ab9184fcd48156db35045868562c6f3ec183900d
Pim Schellart <P.Schellart@princeton.edu>
2016-05-09 11:27:29 -0400

    Use Astropy compatible unit strings

--- python/lsst/meas/deblender/deblend.py
+++ python/lsst/meas/deblender/deblend.py
@@ -175,7 +175,7 @@ class SourceDeblendTask(pipeBase.Task):
         self.psfKey = schema.addField('deblend_deblendedAsPsf', type='Flag',
                                       doc='Deblender thought this source looked like a PSF')
         self.psfCenterKey = afwTable.Point2DKey.addFields(schema, 'deblend_psfCenter',
-                                         'If deblended-as-psf, the PSF centroid', "pixels")
+                                         'If deblended-as-psf, the PSF centroid', "pixel")
         self.psfFluxKey = schema.addField('deblend_psfFlux', type='D',
                                            doc='If deblended-as-psf, the PSF flux')
         self.tooManyPeaksKey = schema.addField('deblend_tooManyPeaks', type='Flag',
