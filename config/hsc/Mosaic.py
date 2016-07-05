f7c8131d03e3f00f25727d7845747697c8582e20
Lauren MacArthur <lauren@astro.princeton.edu>
2016-07-05 13:40:21 -0400

    Adapt config/hsc/Mosaic.py to new colorterm system

--- config/hsc/Mosaic.py
+++ config/hsc/Mosaic.py
@@ -2,7 +2,7 @@ import os.path
 
 from lsst.utils import getPackageDir
 
-config.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "colorterms.py"))
+config.colorterms.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "colorterms.py"))
 config.astrom.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "filterMap.py"))
 
 config.fittingOrder = 9
