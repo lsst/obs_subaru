6eb8971adf3195240c51a70a6fe62eb4e7b46f0b
Lauren MacArthur <lauren@astro.princeton.edu>
2016-07-06 13:55:26 -0400

    configs: Update filterMap loading

--- config/hsc/skyAnalysis.py
+++ config/hsc/skyAnalysis.py
@@ -2,5 +2,6 @@ import os.path
 
 from lsst.utils import getPackageDir
 
-root.colorterms.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "colorterms.py"))
-root.astrometry.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "filterMap.py"))
+config.colorterms.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "colorterms.py"))
+config.refObjLoaderConfig.load(os.path.join(getPackageDir("obs_subaru"), "config", "filterMap.py"))
+config.refObjLoaderConfig.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "filterMap.py"))
