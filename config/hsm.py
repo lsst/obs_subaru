b1409c0d9739205ad01ec0ed366600df764f5346
Lauren MacArthur <lauren@astro.princeton.edu>
2016-08-30 15:20:32 -0400

    config/hsm.py: remove slot definition
    This default is now being set in ext_shapeHSM_HsmSourceMoments's
    config/enable.py.

--- config/hsm.py
+++ config/hsm.py
@@ -6,6 +6,5 @@ from lsst.utils import getPackageDir
 try:
     config.load(os.path.join(getPackageDir("meas_extensions_shapeHSM"), "config", "enable.py"))
     config.plugins["ext_shapeHSM_HsmShapeRegauss"].deblendNChild = "deblend_nChild"
-    config.slots.shape = "ext_shapeHSM_HsmSourceMoments"
 except Exception as e:
     print "Cannot enable shapeHSM (%s): disabling HSM shape measurements" % (e,)
