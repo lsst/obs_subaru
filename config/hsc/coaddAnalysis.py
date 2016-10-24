b0b81607f73b8a45f85b8fd935577fbcf561a4d5
Lauren MacArthur <lauren@astro.princeton.edu>
2016-10-24 12:47:39 -0400

    Add srcSchemaMap to coadd & color analysis configs

--- config/hsc/coaddAnalysis.py
+++ config/hsc/coaddAnalysis.py
@@ -4,3 +4,4 @@ from lsst.utils import getPackageDir
 
 config.colorterms.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "colorterms.py"))
 config.refObjLoaderConfig.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "filterMap.py"))
+config.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "srcSchemaMap.py"))
