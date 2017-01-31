903670bd429453d864abb68c9bc984530d777046
Pim Schellart <P.Schellart@princeton.edu>
2017-01-31 12:08:12 -0500

    Wrap with pybind11 instead of swig

---
+++ python/lsst/obs/subaru/crosstalk.cc
@@ -0,0 +1,44 @@
+/*
+ * LSST Data Management System
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ * See the COPYRIGHT file
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <https://www.lsstcorp.org/LegalNotices/>.
+ */
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "lsst/obs/subaru/Crosstalk.h"
+
+namespace py = pybind11;
+using namespace pybind11::literals;
+
+namespace lsst {
+namespace obs {
+namespace subaru {
+
+PYBIND11_PLUGIN(_crosstalk) {
+    py::module mod("_crosstalk");
+
+    mod.def("subtractCrosstalk", subtractCrosstalk, "mi"_a, "nAmp"_a, "coeffs1List"_a, "coeffs2List"_a,
+            "gainsPreampSig"_a);
+
+    return mod.ptr();
+}
+}
+}
+}  // lsst::obs::subaru
---
+++ python/lsst/obs/subaru/subaruLib.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+import lsst.afw.geom
+
+from ._crosstalk import *
+from ._hscDistortion import *
+
