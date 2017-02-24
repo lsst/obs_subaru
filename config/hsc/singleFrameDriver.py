b9efd23fa53ac0f6067fb76b24a41db2062a9300
Paul Price <price@astro.princeton.edu>
2017-02-24 15:16:49 -0500

    config: ignore focus CCDs in singleFrameDriver
    Otherwise, they can cause errors if they fail the regular processing.

--- config/hsc/singleFrameDriver.py
+++ config/hsc/singleFrameDriver.py
@@ -6,3 +6,4 @@ import os.path
 from lsst.utils import getPackageDir
 
 config.processCcd.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "processCcd.py"))
+config.ignoreCcdList = range(104, 112)
