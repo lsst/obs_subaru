5f3095bd364cd7f7100ba57446090ea9d48a563f
Bob Armstrong <rearmstr@gmail.com>
2017-04-04 19:55:40 -0400

    Switch to selecting images by PSF quality.

--- config/assembleCoadd.py
+++ config/assembleCoadd.py
@@ -3,3 +3,6 @@ config.doMatchBackgrounds = False
 config.doSigmaClip = False
 config.subregionSize = (10000, 200) # 200 rows (since patch width is typically < 10k pixels)
 config.doMaskBrightObjects = True
+
+from lsst.pipe.tasks.selectImages import PsfWcsSelectImagesTask
+config.select.retarget(PsfWcsSelectImagesTask)
