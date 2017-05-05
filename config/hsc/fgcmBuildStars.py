27868976da62405f62cbaa0ca2a0af8f5b994e5d
Eli Rykoff <erykoff@slac.stanford.edu>
2017-05-05 11:15:15 -0700

    HSC defaults for FGCM build stars task

---
+++ config/hsc/fgcmBuildStars.py
@@ -0,0 +1,14 @@
+"""
+HSC-specific overrides for FgcmBuildStars
+"""
+
+config.minPerBand = 2
+config.matchRadius = 1.0
+config.isolationRadius = 2.0
+config.densityCutNside = 128
+config.densityCutMaxPerPixel = 1000
+config.zeropointDefault = 25.0
+config.bands = ('g','r','i','z','y')
+config.requiredFlag = (1,1,1,1,0)
+
+
