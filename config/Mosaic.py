35ac42f53d86362bd14135fd8d779688e1214ee0
Jim Bosch <jbosch@astro.princeton.edu>
2017-05-30 17:16:25 -0400

    Move most meas_mosaic configuration to that package.
    Only filterMap (which we can't move) stays here.

--- config/Mosaic.py
+++ config/Mosaic.py
@@ -1,8 +1,4 @@
 import os
 from lsst.utils import getPackageDir
-from lsst.meas.algorithms import LoadIndexedReferenceObjectsTask
 
-config.loadAstrom.retarget(LoadIndexedReferenceObjectsTask)
-config.loadAstrom.ref_dataset_name = "ps1_pv3_3pi_20170110"
 config.loadAstrom.load(os.path.join(getPackageDir("obs_subaru"), "config", "filterMap.py"))
-config.photoCatName = "ps1_pv3_3pi_20170110"
