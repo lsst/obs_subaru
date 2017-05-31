d6d7589b0ea091a59a7563287d0aaf3bd179dadd
Paul Price <price@astro.princeton.edu>
2017-05-31 15:21:19 -0400

    config: organise filterMap, add missing filters

--- config/hsc/filterMap.py
+++ config/hsc/filterMap.py
@@ -1,3 +1,20 @@
-for source, target in [("N921", 'z'), ("N816", 'i'), ("N515", 'g'), ("N387", 'g'), ("N1010", 'z'),
-                       ("i2", 'i'), ("r2", "r"), ]:
+for source, target in [
+        # Wide bands
+        ("r2", "r"),
+        ("i2", "i"),
+        # Narrow bands
+        ('N387', 'g'),
+        ('N468', 'g'),
+        ('N515', 'g'),
+        ('N527', 'g'),
+        ('N656', 'r'),
+        ('N718', 'i'),
+        ('N816', 'i'),
+        ('N921', 'z'),
+        ('N926', 'z'),
+        ('N973', 'y'),
+        ('N1010', 'y'),
+        # Intermediate bands
+        ('I945', 'z'),
+    ]:
     config.filterMap[source] = target
