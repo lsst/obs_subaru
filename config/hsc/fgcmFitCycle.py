62f3f55879ca495f5085bf83484cec34ea97211b
Eli Rykoff <erykoff@slac.stanford.edu>
2017-08-17 13:48:03 -0700

    Change to simpler sedFudgeFactors

--- config/hsc/fgcmFitCycle.py
+++ config/hsc/fgcmFitCycle.py
@@ -14,6 +14,5 @@ config.cameraGain = 3.0
 config.pixelScale = 0.17
 config.expGrayPhotometricCut = (-0.05,-0.05,-0.05,-0.05,-0.05)
 config.aperCorrFitNBins = 0
-config.sedFitBandFudgeFactors = (1.0,1.0,1.0,1.0)
-config.sedExtraBandFudgeFactors = (1.0,)
+config.sedFudgeFactors = (1.0,1.0,1.0,1.0,1.0)
 config.starColorCuts = ('g,r,-0.25,2.25','r,i,-0.50,2.25','i,z,-0.50,1.00','g,i,0.0,3.5')
