124a8881ff2dd95d6035f3f063e45d36d53a3c70
Eli Rykoff <erykoff@slac.stanford.edu>
2017-10-19 11:27:34 -0700

    Add stdFilterNames.  Fix spacing.

--- config/hsc/fgcmMakeLut.py
+++ config/hsc/fgcmMakeLut.py
@@ -2,18 +2,19 @@
 HSC-specific overrides for FgcmMakeLut
 """
 
-config.filterNames = ('g','r','i','i2','z','y')
-config.pmbRange = (820.0,835.0)
+config.filterNames = ('g', 'r', 'i', 'i2', 'z', 'y')
+config.stdFilterNames = ('g', 'r', 'i2', 'i2', 'z', 'y')
+config.pmbRange = (820.0, 835.0)
 config.pmbSteps = 5
-config.pwvRange = (0.1,3.6)
+config.pwvRange = (0.1, 3.6)
 config.pwvSteps = 11
-config.o3Range = (220.0,310.0)
+config.o3Range = (220.0, 310.0)
 config.o3Steps = 3
-config.tauRange = (0.002,0.25)
+config.tauRange = (0.002, 0.25)
 config.tauSteps = 11
-config.alphaRange = (0.0,2.0)
+config.alphaRange = (0.0, 2.0)
 config.alphaSteps = 9
-config.zenithRange = (0.0,70.0)
+config.zenithRange = (0.0, 70.0)
 config.zenithSteps = 21
 
 config.pmbStd = 828.0
@@ -27,4 +28,4 @@ config.elevation = 4139.0
 
 config.lambdaNorm = 7750.0
 config.lambdaStep = 0.5
-config.lambdaRange = (3900.0,11000.0)
+config.lambdaRange = (3900.0, 11000.0)
