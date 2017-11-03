b03dfd83867dde9ba0e4c2b89d242d30955a78cf
Paul Price <price@astro.princeton.edu>
2017-11-03 17:49:35 +1000

    config: set focal-plane background scale

--- config/hsc/sky.py
+++ config/hsc/sky.py
@@ -6,4 +6,7 @@ from lsst.obs.subaru.isr import SubaruIsrTask
 config.isr.retarget(SubaruIsrTask)
 config.isr.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "isr.py"))
 
+config.largeScaleBackground.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc",
+                                              "focalPlaneBackground.py"))
+
 config.isr.doBrighterFatter = False
