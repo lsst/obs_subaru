2df626cc138c7bf067d41e7ec5cfa9163088d9b9
Paul Price <price@astro.princeton.edu>
2017-11-15 15:50:19 -0500

    config: adapt to removal of background matching
    Background matching has been removed from the AssembleCoaddTask.

--- config/assembleCoadd.py
+++ config/assembleCoadd.py
@@ -1,5 +1,4 @@
 config.badMaskPlanes = ("BAD", "EDGE", "SAT", "INTRP", "NO_DATA",)
-config.doMatchBackgrounds = False
 config.doSigmaClip = False
 config.subregionSize = (10000, 200) # 200 rows (since patch width is typically < 10k pixels)
 config.doMaskBrightObjects = True
