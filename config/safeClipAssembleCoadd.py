4c4930026b042d1c4d56eba1c1668f97794ed8c7
Yusra AlSayyad <yusra@astro.princeton.edu>
2017-11-15 15:04:33 -0600

    Turn on doNImage by default

--- config/safeClipAssembleCoadd.py
+++ config/safeClipAssembleCoadd.py
@@ -3,3 +3,4 @@ config.doMatchBackgrounds = False
 config.subregionSize = (10000, 200) # 200 rows (since patch width is typically < 10k pixels
 config.doMaskBrightObjects = True
 config.removeMaskPlanes.append("CROSSTALK")
+config.doNImage = True
