3e11e83f4b80aaeb6ff32503fef931590b8fee64
Yusra AlSayyad <yusra@astro.princeton.edu>
2017-12-10 18:52:54 -0600

    Apply pep8 formatting

--- config/compareWarpAssembleCoadd.py
+++ config/compareWarpAssembleCoadd.py
@@ -1,7 +1,8 @@
 config.badMaskPlanes = ("BAD", "EDGE", "SAT", "INTRP", "NO_DATA",)
-config.subregionSize = (10000, 200) # 200 rows (since patch width is typically < 10k pixels
+config.subregionSize = (10000, 200)  # 200 rows (since patch width is typically < 10k pixels
 config.doMaskBrightObjects = True
 config.doNImage = True
-                                                                                                                                                                        
+
 config.assembleStaticSkyModel.badMaskPlanes = ("BAD", "EDGE", "SAT", "INTRP", "NO_DATA",)
-config.assembleStaticSkyModel.subregionSize = (10000, 200) # 200 rows (since patch width is typically < 10k pixels  
+# 200 rows (since patch width is typically < 10k pixels
+config.assembleStaticSkyModel.subregionSize = (10000, 200)
