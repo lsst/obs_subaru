7463216a9d90947f5f63357f6b835e9e1e6cfeb9
Yusra AlSayyad <yusra@astro.princeton.edu>
2017-12-10 18:48:59 -0600

    Apply DM-12665 to CompareWarp too

--- config/compareWarpAssembleCoadd.py
+++ config/compareWarpAssembleCoadd.py
@@ -1,6 +1,7 @@
 config.badMaskPlanes = ("BAD", "EDGE", "SAT", "INTRP", "NO_DATA",)
 config.subregionSize = (10000, 200)  # 200 rows (since patch width is typically < 10k pixels
 config.doMaskBrightObjects = True
+config.removeMaskPlanes.append("CROSSTALK")
 config.doNImage = True
 
 # 200 rows (since patch width is typically < 10k pixels
