b8ffd371efaa0223011b551357d6e7eb4bdce738
Yusra AlSayyad <yusra@astro.princeton.edu>
2017-12-10 18:45:41 -0600

    Allow bad pixels (except NO_DATA) in static sky model
    Removing all types of bad pixels (e.g. SAT, INTRP) from the
    PSF-Matched template coadd can remove the last available epochs
    from the template for small numbers of epochs. Bad pixels
    are still removed from the resulting CompareWarp Coadd.

--- config/compareWarpAssembleCoadd.py
+++ config/compareWarpAssembleCoadd.py
@@ -3,6 +3,5 @@ config.subregionSize = (10000, 200)  # 200 rows (since patch width is typically 
 config.doMaskBrightObjects = True
 config.doNImage = True
 
-config.assembleStaticSkyModel.badMaskPlanes = ("BAD", "EDGE", "SAT", "INTRP", "NO_DATA",)
 # 200 rows (since patch width is typically < 10k pixels
 config.assembleStaticSkyModel.subregionSize = (10000, 200)
