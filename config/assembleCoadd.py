12861cc56c40f6ef59c14aefe6e8d5710dee6e33
Jim Bosch <jbosch@astro.princeton.edu>
2017-12-12 11:28:11 -0500

    Remove INTERP from bad mask planes for coaddition
    Interpolation is pretty good for e.g. cosmic rays, and
    leaving out pixels leads to INEXACT_PSF masks on the coadd,
    which we'd like to minimize.

--- config/assembleCoadd.py
+++ config/assembleCoadd.py
@@ -1,4 +1,4 @@
-config.badMaskPlanes = ("BAD", "EDGE", "SAT", "INTRP", "NO_DATA",)
+config.badMaskPlanes = ("BAD", "EDGE", "SAT", "NO_DATA",)
 config.doSigmaClip = False
 config.subregionSize = (10000, 200) # 200 rows (since patch width is typically < 10k pixels)
 config.doMaskBrightObjects = True
