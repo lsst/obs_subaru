afe70096beff2b676cc1e23332e91a8e1d97d348
Eli Rykoff <erykoff@slac.stanford.edu>
2018-01-11 07:50:38 -0800

    Add HSC-specific overrides for FgcmMakeLut, using default table.

---
+++ config/fgcmMakeLut.py
@@ -0,0 +1,7 @@
+"""
+HSC-specific overrides for FgcmMakeLut
+"""
+
+config.filterNames = ('g', 'r', 'i', 'i2', 'z', 'y')
+config.stdFilterNames = ('g', 'r', 'i2', 'i2', 'z', 'y')
+config.atmosphereTableName = 'fgcm_atm_subaru1'
