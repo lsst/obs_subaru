a4746703034c9d4459791f4d6646a9523b0a8ba7
Paul Price <price@astro.princeton.edu>
2018-01-15 23:24:48 -0500

    config: update Suprime-Cam configuration
    Required following reworking of ProcessCcdTask some time ago.

--- config/suprimecam/processCcd.py
+++ config/suprimecam/processCcd.py
@@ -8,7 +8,7 @@ from lsst.utils import getPackageDir
 
 suprimecamConfigDir = os.path.join(getPackageDir("obs_subaru"), "config", "suprimecam")
 config.load(os.path.join(suprimecamConfigDir, 'isr.py'))
-config.calibrate.photocal.colorterms.load(os.path.join(suprimecamConfigDir, 'colorterms.py'))
+config.calibrate.photoCal.colorterms.load(os.path.join(suprimecamConfigDir, 'colorterms.py'))
 
 config.charImage.measurement.plugins["base_Jacobian"].pixelScale = 0.2
 config.calibrate.measurement.plugins["base_Jacobian"].pixelScale = 0.2
