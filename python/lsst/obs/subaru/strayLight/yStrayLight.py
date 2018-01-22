a5d3cff34d9a91ca2da7536b0848fff5b893f65d
Paul Price <price@astro.princeton.edu>
2018-01-22 16:59:36 -0500

    strayLight: set zero correction if detector is bad
    ccd=9 is all bad, and setting it to the median of the good
    pixels resulted in NANs. Instead, set the correction to zero,
    which doesn't kill downstream processing.

--- python/lsst/obs/subaru/strayLight/yStrayLight.py
+++ python/lsst/obs/subaru/strayLight/yStrayLight.py
@@ -116,9 +116,12 @@ class StrayLightTask(Task):
                 amp = exposure.getDetector()[ii]
                 box = amp.getBBox()
                 isBad[box.getBeginY():box.getEndY(), box.getBeginX():box.getEndX()] = True
-            model[isBad] = numpy.median(model[~isBad])
             mask = exposure.getMaskedImage().getMask()
             mask.array[isBad] |= mask.getPlaneBitMask("BAD")
+            if numpy.all(isBad):
+                model[:] = 0.0
+            else:
+                model[isBad] = numpy.median(model[~isBad])
 
         model *= exposure.getInfo().getVisitInfo().getExposureTime()
         exposure.image.array -= model
