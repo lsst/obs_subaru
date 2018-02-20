809a097f87b260b4fb8d64b01c5faa3d3d3349d1
Yusra AlSayyad <yusra@astro.princeton.edu>
2018-02-19 23:05:48 -0600

    Adapt to coaddBase matchingKernelSize config

--- config/assembleCoadd.py
+++ config/assembleCoadd.py
@@ -1,3 +1,9 @@
+import os.path
+from lsst.utils import getPackageDir
+
+# Load configs shared between assembleCoadd and makeCoaddTempExp
+config.load(os.path.join(getPackageDir("obs_subaru"), "config", "coaddBase.py"))
+
 config.doSigmaClip = False
 config.subregionSize = (10000, 200) # 200 rows (since patch width is typically < 10k pixels)
 config.doMaskBrightObjects = True
---
+++ config/coaddBase.py
@@ -0,0 +1,2 @@
+# Configs shared between makeCoaddTempExp and assemble
+config.matchingKernelSize = 29
