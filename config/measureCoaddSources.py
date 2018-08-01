f64ef0b2d1d09410cda6188af6300c4e8fee93e1
fred3m <fredem@princeton.edu>
2018-08-01 16:02:30 -0500

    Configure for new DeblendCoaddSourcesTask

--- config/measureCoaddSources.py
+++ config/measureCoaddSources.py
@@ -10,8 +10,6 @@ config.measurement.load(os.path.join(getPackageDir("obs_subaru"), "config", "con
 config.measurement.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsm.py"))
 config.load(os.path.join(getPackageDir("obs_subaru"), "config", "cmodel.py"))
 
-config.deblend.load(os.path.join(getPackageDir("obs_subaru"), "config", "deblend.py"))
-
 config.match.refObjLoader.retarget(LoadIndexedReferenceObjectsTask)
 config.match.refObjLoader.ref_dataset_name = "ps1_pv3_3pi_20170110"
 config.match.refObjLoader.load(os.path.join(getPackageDir("obs_subaru"), "config", "filterMap.py"))
