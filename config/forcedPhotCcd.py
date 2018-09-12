897834736060e21644775681a38c3ea507dae6ca
Paul Price <price@astro.princeton.edu>
2018-09-12 14:10:52 -0400

    config: disable CModel in forcedPhotCcd
    It doesn't work!
    This line has been there for a while, but CModel is explicitly disabled
    in ci_hsc when it does forcedPhotCcd, so I conclude this line was added
    in error.

--- config/forcedPhotCcd.py
+++ config/forcedPhotCcd.py
@@ -4,6 +4,5 @@ from lsst.utils import getPackageDir
 
 config.measurement.load(os.path.join(getPackageDir("obs_subaru"), "config", "apertures.py"))
 config.measurement.load(os.path.join(getPackageDir("obs_subaru"), "config", "kron.py"))
-config.load(os.path.join(getPackageDir("obs_subaru"), "config", "cmodel.py"))
 
 config.measurement.slots.instFlux = None
