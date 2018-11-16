834012e385ebf9ef50b6d6800107a95ad1a93cd0
Christopher Morrrison <morriscb@uw.edu>
2018-11-15 17:41:55 -0800

    Remove association config file.

--- config/hsc/apPipe.py
+++ config/hsc/apPipe.py
@@ -5,5 +5,3 @@ from lsst.utils import getPackageDir
 hscConfigDir = os.path.join(getPackageDir('obs_subaru'), 'config', 'hsc')
 
 config.ccdProcessor.load(os.path.join(hscConfigDir, "processCcd.py"))
-config.associator.load(os.path.join(hscConfigDir, "association.py"))
-
