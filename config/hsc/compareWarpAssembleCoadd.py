614fbe47ba28a080f2d80c4afc7c2408b44ee5ca
John K. Parejko <parejkoj@uw.edu>
2018-11-20 14:36:39 -0800

    Add useMeasMosaic=True while DRP looks at differences

--- config/hsc/compareWarpAssembleCoadd.py
+++ config/hsc/compareWarpAssembleCoadd.py
@@ -5,3 +5,6 @@ from lsst.utils import getPackageDir
 config.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "assembleCoadd.py"))
 
 config.assembleStaticSkyModel.doApplyUberCal = True
+# Continue to default to the meas_mosaic output while the DRP team investigates
+# differences between it and jointcal.
+config.useMeasMosaic = True
