0fbb387c21ca22778ebf6cf96a3df2fe76e9b1a4
Lauren MacArthur <lauren@astro.princeton.edu>
2019-01-18 18:22:40 -0600

    Formatting updates for bin.src/displayCamera.py

--- bin.src/displayCamera.py
+++ bin.src/displayCamera.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+
+# This file is part of obs_subaru.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import argparse
 import re
 import sys
@@ -8,10 +30,24 @@ import lsst.afw.display as afwDisplay
 
 
 def checkStr(strVal, level):
-    """Check if a string is a valid identifier
-    @param[in] strVal: String containing the identifier
-    @param[in] level: level of identifier: amp, ccd, raft
-    return True if valid
+    """Check if a string is a valid identifier.
+
+    Parameters
+    ----------
+    strVal : `str`
+       String containing the identifier
+    level : `str`
+       Level of identifier: amp, ccd, raft.
+
+    Raises
+    ------
+    ValueError
+       If unknown combination of ``strVal`` and ``level`` are provided.
+
+    Returns
+    -------
+    result : `bool`
+       Returns `True` if valid.
     """
     if level == 'amp':
         matchStr = '^[01]_[0-9]* [0-3],0$'
@@ -30,8 +66,12 @@ def checkStr(strVal, level):
 
 
 def displayCamera(args):
-    """Display camera element according to command-line arguments
-    @param[in] args: argparse.Namespace list of command-line arguments
+    """Display camera element according to command-line arguments.
+
+    Parameters
+    ----------
+    args : `argparse.Namespace`
+       Command-line arguments to parse.
     """
     mapper = obs_hsc.HscMapper(root=".")
     camera = mapper.camera
@@ -91,16 +131,14 @@ def displayCamera(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Display the Subaru cameras')
-    parser.add_argument('--showAmp', help='Show an amplifier segment in ds9  May have multiple arguments. '
-                                          'Format like ccd_name amp_name e.g. '
-                                          '\"1_53 0,0\"', type=str, nargs='+')
-    parser.add_argument('--showCcd', help='Show a CCD from the mosaic in ds9.  May have multiple arguments. '
-                                          'Format like ccd_name e.g. \"0_17\"', type=str,
-                                          nargs='+')
-    parser.add_argument('--showRaft', help='Show a raft from the mosaic in ds9.'
-                        ' May have multiple arguments. '
-                        'May be 0 or 1', type=str)
-    parser.add_argument('--showCamera', help='Show the camera mosaic in ds9.', action='store_true')
+    parser.add_argument('--showAmp', help='Show an amplifier segment.  May have multiple arguments.  '
+                                          'Format like ccd_name amp_name e.g. \"1_53 0,0\"',
+                        type=str, nargs='+')
+    parser.add_argument('--showCcd', help='Show a CCD from the mosaic.  May have multiple arguments.  '
+                                          'Format like ccd_name e.g. \"0_17\"', type=str, nargs='+')
+    parser.add_argument('--showRaft', help='Show a raft from the mosaic.  May have multiple arguments.  '
+                                           'May be 0 or 1', type=str, nargs='+')
+    parser.add_argument('--showCamera', help='Show the camera mosaic.', action='store_true')
     parser.add_argument('--cameraBinSize', type=int, default=20,
                         help='Size of binning when displaying the full camera mosaic')
     parser.add_argument('--plotFocalPlane', action='store_true',
