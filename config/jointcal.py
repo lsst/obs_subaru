5ff6872c690ec7323e03519a28f0183621e6b509
John Parejko <parejkoj@uw.edu>
2019-05-05 22:51:21 -0700

    Add reference catalog coordinate error override

--- config/jointcal.py
+++ config/jointcal.py
@@ -5,6 +5,8 @@ if hasattr(config.astrometryRefObjLoader, "ref_dataset_name"):
     config.astrometryRefObjLoader.ref_dataset_name = 'ps1_pv3_3pi_20170110'
 if hasattr(config.photometryRefObjLoader, "ref_dataset_name"):
     config.photometryRefObjLoader.ref_dataset_name = 'ps1_pv3_3pi_20170110'
+# existing PS1 refcat does not have coordinate errors
+config.astrometryReferenceErr = 10
 
 filterMapFile = os.path.join(getPackageDir("obs_subaru"), "config", "filterMap.py")
 config.photometryRefObjLoader.load(filterMapFile)
