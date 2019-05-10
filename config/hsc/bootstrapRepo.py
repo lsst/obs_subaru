f42b9207d5d23f16221d3baf9c830ea9dd62e4c7
Jim Bosch <jbosch@astro.princeton.edu>
2019-05-10 12:13:31 -0400

    Default collection for Gen3 skymaps is now defined in obs_base.

--- config/hsc/bootstrapRepo.py
+++ config/hsc/bootstrapRepo.py
@@ -2,7 +2,7 @@ import os.path
 from lsst.utils import getPackageDir
 from lsst.obs.base.gen3 import BootstrapRepoSkyMapConfig
 
-config.skymaps["hsc_rings_v1"] = BootstrapRepoSkyMapConfig(collection="skymaps")
+config.skymaps["hsc_rings_v1"] = BootstrapRepoSkyMapConfig()
 config.skymaps["hsc_rings_v1"].load(os.path.join(getPackageDir("obs_subaru"), "config",
                                                  "makeSkyMap.py"))
 config.skymaps["hsc_rings_v1"].load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc",
