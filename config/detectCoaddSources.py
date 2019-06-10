2e098d7e9023413fb7dd1e259695669b2583e3bb
Paul Price <price@astro.princeton.edu>
2019-06-10 14:30:38 -0400

    config: reduce background mesh size for detection on coadds
    We have determined that we want to use a binSize=128 pixels
    for the background mesh in coadd detection. This will reduce
    the load on the deblender, and optimise the quality of the
    detection and photometry for small (almost-)unresolved sources.
    Those interested in science on large sources can restore the
    background and make their own specialist measurements.
    
    This change was provided by Hiroyuki Ikeda.

---
+++ config/detectCoaddSources.py
@@ -0,0 +1,6 @@
+config.detection.doTempWideBackground =   True
+config.detection.tempWideBackground.binSize = 128
+config.detection.tempWideBackground.useApprox = False
+config.detection.reEstimateBackground = True
+config.detection.background.binSize = 128
+config.detection.background.useApprox = False
