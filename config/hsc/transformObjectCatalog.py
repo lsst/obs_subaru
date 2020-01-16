1a7d92219714044140d9db9e94b283bfe109f434
Hsin-Fang Chiang <hchiang@lsst.org>
2020-01-16 15:27:09 -0600

    Tweak the filter list for transformObjectCatalog
    This ticket changes how this config.filterMap is used in
    determining the output schema. With this change, only the
    filters we want to include in the output should be listed
    in this task config.

--- config/hsc/transformObjectCatalog.py
+++ config/hsc/transformObjectCatalog.py
@@ -1,5 +1,7 @@
 import os
 from lsst.utils import getPackageDir
 
-config.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "filterMap.py"))
+for filt, filtShort in [("HSC-G", "g"), ("HSC-R", "r"), ("HSC-I", "i"),
+                        ("HSC-Z", "z"), ("HSC-Y", "y")]:
+    config.filterMap[filt] = filtShort
 config.functorFile = os.path.join(getPackageDir("obs_subaru"), 'policy', 'Object.yaml')
