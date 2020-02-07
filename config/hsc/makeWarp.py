7dc914ec664729aa84c81b0076c0f434a4a190d7
Paul Price <price@astro.princeton.edu>
2020-02-07 15:54:50 -0500

    config: add makeWarp
    Gen3 verison of makeCoaddTempExp is called makeWarp.

---
+++ config/hsc/makeWarp.py
@@ -0,0 +1,11 @@
+import os.path
+from lsst.utils import getPackageDir
+
+# Load configs shared between assembleCoadd and makeCoaddTempExp
+config.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "coaddBase.py"))
+
+config.recalibrate.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "recalibrate.py"))
+
+config.doApplySkyCorr = True
+config.warpAndPsfMatch.warp.warpingKernelName = 'lanczos5'
+config.coaddPsf.warpingKernelName = 'lanczos5'
