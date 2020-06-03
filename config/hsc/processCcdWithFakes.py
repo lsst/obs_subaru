334b644196a36fd022e33fe28756c5515313166e
Sophie Reed <sophiereed@princeton.edu>
2020-06-03 17:12:47 -0500

    Update processCcdWithFakes configs to inherit from calibrate.

--- config/hsc/processCcdWithFakes.py
+++ config/hsc/processCcdWithFakes.py
@@ -1,12 +1,11 @@
 """
 HSC-specific overrides for ProcessCcdWithFakesTask
 (applied after Subaru overrides in ../processCcdWithFakes.py).
-Should be kept up to date with config/hsc/calibrate.py so that
-processCcd and processCcdWithFakes produce the same output.
 """
 import os.path
 
-
 ObsConfigDir = os.path.dirname(__file__)
 
-config.measurement.plugins["base_Jacobian"].pixelScale = 0.168
+calFile = os.path.join(ObsConfigDir, "calibrate.py")
+
+config.calibrate.load(calFile)
