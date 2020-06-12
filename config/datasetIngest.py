4ce04a0159cf7a242c1df3a4e828f7b28c17cffb
Krzysztof Findeisen <kfindeis@uw.edu>
2020-06-12 15:39:03 -0500

    Rename DatasetIngestConfig fields to not be defect-specific.

--- config/datasetIngest.py
+++ config/datasetIngest.py
@@ -7,8 +7,8 @@ from lsst.obs.subaru.ingest import HscIngestTask
 configDir = os.path.dirname(__file__)
 defectDir = os.path.join(getPackageDir('obs_subaru_data'), 'hsc', 'defects')
 
-config.textDefectPath = defectDir
+config.curatedCalibPaths = defectDir
 config.dataIngester.retarget(HscIngestTask)
 config.dataIngester.load(os.path.join(configDir, 'ingest.py'))
 config.calibIngester.load(os.path.join(configDir, 'ingestCalibs.py'))
-config.defectIngester.load(os.path.join(configDir, 'ingestCuratedCalibs.py'))
+config.curatedCalibIngester.load(os.path.join(configDir, 'ingestCuratedCalibs.py'))
