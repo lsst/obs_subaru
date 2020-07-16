cc384e0fa17707d3e88ca80f753be9686c7b42ce
John Parejko <parejkoj@uw.edu>
2020-07-16 01:02:19 -0700

    Use jointcal refcat defaults (Gaia DR2 astrometry, PS1 photometry)
    This changes from the previous PS1 astrometry that HSC had been using until
    now.
    Also cleanup config/hsc/jointcal.py vs. config/jointcal.py: the latter is now
    entirely superfluous.

--- config/hsc/jointcal.py
+++ config/hsc/jointcal.py
@@ -1,15 +1,15 @@
 import os.path
 
-# `load()` appends to the filterMaps: we need them to be empty, so that only
-# the specified filter mappings are used.
+# `load()` appends to the filterMaps: we need them to be empty for HSC, so that
+# only the specified filter mappings are used.
 config.photometryRefObjLoader.filterMap = {}
-config.astrometryRefObjLoader.filterMap = {}
 filterMapFile = os.path.join(os.path.dirname(__file__), "filterMap.py")
 config.photometryRefObjLoader.load(filterMapFile)
-config.astrometryRefObjLoader.load(filterMapFile)
-# jointcal default is for Gaia DR2, so we need to clear `anyFilterMapsToThis`.
-config.astrometryRefObjLoader.anyFilterMapsToThis = None
-
+# We have PS1 colorterms for HSC.
 config.applyColorTerms = True
-
 config.colorterms.load(os.path.join(os.path.dirname(__file__), "colorterms.py"))
+
+# HSC needs a higher order polynomial to track the steepness of the optical
+# distortions along the edge of the field. Emperically, this provides a
+# measurably better fit than the default order=5.
+config.astrometryVisitOrder = 7
