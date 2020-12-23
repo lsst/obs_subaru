249c102d97a7f4ea24c5067641035119600de7d5
Ian <sullii@uw.edu>
2020-12-22 19:48:00 -0600

    Load camera-specific configs for SafeClipAssemble subtasks

--- config/safeClipAssembleCoadd.py
+++ config/safeClipAssembleCoadd.py
@@ -2,3 +2,9 @@ import os.path
 
 # Load configs from base assembleCoadd
 config.load(os.path.join(os.path.dirname(__file__), "assembleCoadd.py"))
+
+# Load configs for the subtasks
+config.assembleMeanCoadd.load(os.path.join(os.path.dirname(__file__), "assembleCoadd.py"))
+config.assembleMeanCoadd.statistic = 'MEAN'
+config.assembleMeanClipCoadd.load(os.path.join(os.path.dirname(__file__), "assembleCoadd.py"))
+config.assembleMeanClipCoadd.statistic = 'MEANCLIP'
