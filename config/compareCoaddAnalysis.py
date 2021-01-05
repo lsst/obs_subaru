c996e8cb031fbb719dd1f6d95a3c0f5501664133
Lauren MacArthur <lauren@astro.princeton.edu>
2021-01-05 15:28:55 -0600

    Update ref loader names in pipe_analysis configs
    This reflects that we now use seperate loaders for the astrometric
    and photometric match-to-reference catalogs for QA assessments.
    
    Also updates the compareVisitAnalysis script to use fgcm as the
    defaults for the external photometric calibrations.

--- config/compareCoaddAnalysis.py
+++ config/compareCoaddAnalysis.py
@@ -1,5 +1,6 @@
 import os.path
 
 config.colorterms.load(os.path.join(os.path.dirname(__file__), "colorterms.py"))
-config.refObjLoader.load(os.path.join(os.path.dirname(__file__), "filterMap.py"))
+config.astromRefObjLoader.load(os.path.join(os.path.dirname(__file__), "filterMap.py"))
+config.photomRefObjLoader.load(os.path.join(os.path.dirname(__file__), "filterMap.py"))
 config.load(os.path.join(os.path.dirname(__file__), "srcSchemaMap.py"))
--- config/compareVisitAnalysis.py
+++ config/compareVisitAnalysis.py
@@ -1,4 +1,8 @@
 import os.path
 
-config.refObjLoader.load(os.path.join(os.path.dirname(__file__), "filterMap.py"))
+config.astromRefObjLoader.load(os.path.join(os.path.dirname(__file__), "filterMap.py"))
+config.photomRefObjLoader.load(os.path.join(os.path.dirname(__file__), "filterMap.py"))
 config.load(os.path.join(os.path.dirname(__file__), "srcSchemaMap.py"))
+
+config.externalPhotoCalibName1 = 'fgcm'
+config.externalPhotoCalibName2 = 'fgcm'
