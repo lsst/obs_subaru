60a62699a31b2636066d42aa212140a189ccc087
Yusra AlSayyad <yusra@astro.princeton.edu>
2021-02-16 23:11:29 -0600

    Normalize DRP pipeline
    in preparation for running on other cameras:
      - Move gen3 specific configs to pipe_tasks
      - Make override files in config/ for tasks that changed
        DefaultName
      - Remove configs redundant with those in config/ now that
        they are picked up by pipetask run

---
+++ config/makeWarp.py
@@ -0,0 +1,12 @@
+import os.path
+
+# Load configs shared between assembleCoadd and makeWarp
+config.load(os.path.join(os.path.dirname(__file__), "coaddBase.py"))
+
+config.makePsfMatched = True
+config.doApplySkyCorr = True
+
+config.modelPsf.defaultFwhm = 7.7
+config.warpAndPsfMatch.psfMatch.kernel['AL'].alardSigGauss = [1.0, 2.0, 4.5]
+config.warpAndPsfMatch.warp.warpingKernelName = 'lanczos5'
+config.coaddPsf.warpingKernelName = 'lanczos5'
