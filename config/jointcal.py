6cf07ad06f7ef0effad770f3e768c75e56efe776
Clare Saunders <cmsaunders@princeton.edu>
2021-06-14 15:22:51 -0500

    Set outlier tolerance parameter for jointcal

--- config/jointcal.py
+++ config/jointcal.py
@@ -13,3 +13,8 @@ config.colorterms.load(os.path.join(os.path.dirname(__file__), "colorterms.py"))
 # distortions along the edge of the field. Emperically, this provides a
 # measurably better fit than the default order=5.
 config.astrometryVisitOrder = 7
+
+# For the HSC deep fields with hundreds of visits, outlier rejection takes ~two
+# weeks of runtime. By stopping outlier rejection when it ceases to have a
+# significant effect on the model, we can bring compute time down to a few days.
+config.astrometryOutlierRelativeTolerance = 0.002
