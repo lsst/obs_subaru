00a0480f77407205e9d430eb171e1395e876f9b1
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2021-10-13 16:57:07 -0500

    Add additional GAaP columns

--- config/gaap.py
+++ config/gaap.py
@@ -3,6 +3,7 @@
 try:
     import lsst.meas.extensions.gaap  # noqa
     config.plugins.names.add("ext_gaap_GaapFlux")
+    config.plugins["ext_gaap_GaapFlux"].sigmas = [0.5, 0.7, 1.0, 1.5, 2.5, 3.0]
     # Enable PSF photometry after PSF-Gaussianization in the `ext_gaap_GaapFlux` plugin
     config.plugins["ext_gaap_GaapFlux"].doPsfPhotometry = True
 except ImportError as exc:
