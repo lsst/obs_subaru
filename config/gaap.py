0b775f9a6d69abff1ce9e2de92a393b30836442d
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2021-10-13 16:56:32 -0500

    Move PSF photometry to gaap.py

--- config/gaap.py
+++ config/gaap.py
@@ -3,5 +3,7 @@
 try:
     import lsst.meas.extensions.gaap  # noqa
     config.plugins.names.add("ext_gaap_GaapFlux")
+    # Enable PSF photometry after PSF-Gaussianization in the `ext_gaap_GaapFlux` plugin
+    config.plugins["ext_gaap_GaapFlux"].doPsfPhotometry = True
 except ImportError as exc:
     print("Cannot import lsst.meas.extensions.gaap (%s): disabling GAaP flux measurements" % (exc,))
