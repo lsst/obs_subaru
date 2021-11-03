fc00ed3b64c70d45ae3d46a00f3a2a2438a9de52
Yusra AlSayyad <yusra@astro.princeton.edu>
2021-11-03 14:30:11 -0500

    Move yaml specifications for column transformation
    These were moved pipe_tasks/schemas/

--- config/transformObjectCatalog.py
+++ config/transformObjectCatalog.py
@@ -1,11 +1,3 @@
-import os.path
-
-from lsst.utils import getPackageDir
-
 # Always produce these output bands in the Parquet coadd tables, no matter
 # what bands are in the input.
 config.outputBands = ["g", "r", "i", "z", "y"]
-
-# Use the environment variable to prevent hardcoding of paths
-# into quantum graphs.
-config.functorFile = os.path.join('$OBS_SUBARU_DIR', 'policy', 'Object.yaml')
--- config/transformSourceTable.py
+++
@@ -1,5 +0,0 @@
-import os.path
-
-# Use the environment variable to prevent hardcoding of paths
-# into quantum graphs.
-config.functorFile = os.path.join('$OBS_SUBARU_DIR', 'policy', 'Source.yaml')
