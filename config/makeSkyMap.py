c7d7e59be8f5e94a13853302d89201aa8ec57de0
Lee Kelvin <lkelvin@astro.princeton.edu>
2022-05-24 10:45:56 -0500

    Clean up makeSkyMap config script

--- config/makeSkyMap.py
+++ config/makeSkyMap.py
@@ -1,21 +1,22 @@
-config.skyMap = "discrete"
+config.skyMap = "rings"
 
 # Configuration for HealpixSkyMap
 config.skyMap["healpix"].log2NSide = 5
 config.skyMap["healpix"].pixelScale = 0.18
-config.skyMap["healpix"].patchBorder = 333 # Pixels
-config.skyMap["healpix"].tractOverlap = 2.0/60 # Degrees
+config.skyMap["healpix"].patchBorder = 333  # pixels
+config.skyMap["healpix"].tractOverlap = 2.0/60  # degrees
 config.skyMap["healpix"].projection = "TAN"
 
 # Configuration for DiscreteSkyMap
 # 0: ACTJ0022M0036
 # 1: M31
-config.skyMap["discrete"].raList = [5.5, 10.7]
-config.skyMap["discrete"].decList = [-0.6, 41.3]
-config.skyMap["discrete"].radiusList = [0.5, 0.8]
-config.skyMap["discrete"].pixelScale = 0.168
-config.skyMap.name = "rings"
+config.skyMap["discrete"].raList = [5.5, 10.7]  # degrees
+config.skyMap["discrete"].decList = [-0.6, 41.3]  # degrees
+config.skyMap["discrete"].radiusList = [0.5, 0.8]  # degrees
+config.skyMap["discrete"].pixelScale = 0.168  # arcsec/pixel
+
+# Configuration for RingsSkyMap
 config.skyMap["rings"].numRings = 120
 config.skyMap["rings"].projection = "TAN"
-config.skyMap["rings"].tractOverlap = 1.0/60 # Overlap between tracts (degrees)
-config.skyMap["rings"].pixelScale = 0.168
+config.skyMap["rings"].tractOverlap = 1.0/60  # Overlap between tracts (degrees)
+config.skyMap["rings"].pixelScale = 0.168  # arcsec/pixel
