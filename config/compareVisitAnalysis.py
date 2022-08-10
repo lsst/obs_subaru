9e02b2e18bd57bac241d8c9d67d73ceb7b6860b1
Tim Jenness <tjenness@lsst.org>
2022-08-10 14:36:12 -0700

    Remove pipe_analysis configs

--- config/compareVisitAnalysis.py
+++
@@ -1,8 +0,0 @@
-import os.path
-
-config.astromRefObjLoader.load(os.path.join(os.path.dirname(__file__), "filterMap.py"))
-config.photomRefObjLoader.load(os.path.join(os.path.dirname(__file__), "filterMap.py"))
-config.load(os.path.join(os.path.dirname(__file__), "srcSchemaMap.py"))
-
-config.externalPhotoCalibName1 = 'fgcm'
-config.externalPhotoCalibName2 = 'fgcm'
