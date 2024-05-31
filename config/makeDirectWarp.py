974fa2b38c199c0d27f9ec0e96218f0199239b8f
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2024-05-30 20:09:26 -0400

    Split warping configs

---
+++ config/makeDirectWarp.py
@@ -0,0 +1,5 @@
+config.doApplyNewBackground = True
+
+config.warper.warpingKernelName = 'lanczos5'
+config.coaddPsf.warpingKernelName = 'lanczos5'
+
---
+++ config/makePsfMatchedWarp.py
@@ -0,0 +1,5 @@
+config.matchingKernelSize = 29
+
+config.modelPsf.defaultFwhm = 7.7
+config.psfMatch.kernel['AL'].alardSigGauss = [1.0, 2.0, 4.5]
+
