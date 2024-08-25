75272c8bf672bcc1b60ecfa921f4f6785af79a01
Lauren MacArthur <lauren@astro.princeton.edu>
2024-08-25 13:41:32 -0700

    Split out fiducial config pars to individual files

---
+++ config/fiducialPsfSigma.py
@@ -0,0 +1,15 @@
+# Derived from HSC PDR2 reprocessing v24.1.0 (DM-39132)
+# These are the median values of quality-selected exposures.
+
+# Fiducial PsfSigma in pixels
+config.fiducialPsfSigma = {
+    "u": 2.0,
+    "g": 2.04,
+    "r": 1.86,
+    "i": 1.51,
+    "z": 1.75,
+    "y": 1.88,
+    "N387": 1.84,
+    "N816": 1.82,
+    "N921": 1.70,
+}
---
+++ config/fiducialZeroPoint.py
@@ -0,0 +1,15 @@
+# Derived from HSC PDR2 reprocessing v24.1.0 (DM-39132)
+# These are the median values of quality-selected exposures.
+
+# Fiducial ZeroPoint for 1s exposure
+config.fiducialZeroPoint = {
+    "u": 26.0,
+    "g": 27.05,
+    "r": 27.10,
+    "i": 26.94,
+    "z": 26.07,
+    "y": 25.63,
+    "N387": 22.24,
+    "N816": 23.94,
+    "N921": 24.09,
+}
