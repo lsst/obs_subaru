a2b505f8bf8f41ecc2836bdaa3e8f56c91550835
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2025-04-23 17:53:17 -0400

    Remove makeWarp config

--- config/makeWarp.py
+++
@@ -1,8 +0,0 @@
-config.makePsfMatched = True
-config.doApplySkyCorr = True
-
-config.modelPsf.defaultFwhm = 7.7
-config.warpAndPsfMatch.psfMatch.kernel["AL"].kernelSize = 29
-config.warpAndPsfMatch.psfMatch.kernel["AL"].alardSigGauss = [1.0, 2.0, 4.5]
-config.warpAndPsfMatch.warp.warpingKernelName = "lanczos5"
-config.coaddPsf.warpingKernelName = "lanczos5"
